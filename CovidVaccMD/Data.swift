3625a61ff407eb966d3e8edee25069618e08d761
Matthew Garlington <SBMatthewG@gmail.com>
2021-02-18 10:59:18 -0500

    feat: Added Class For API Connection to MD Gov OpenData JSON file on Maryland Vaccine information
    This Class was added along with data to process the API Request

--- CovidVaccMD/Data.swift
+++ CovidVaccMD/Data.swift
@@ -26,12 +26,12 @@ class Api {
         URLSession.shared.dataTask(with: url) { (data, _, _) in
             
             
-        // Prevent Crashing by taking away the ! in data below with adding guard let
-        guard let data = data else { return }
+            // Prevent Crashing by taking away the ! in data below with adding guard let
+            guard let data = data else { return }
             
-        let posts = try! JSONDecoder().decode([Post].self, from: data)
+            let posts = try! JSONDecoder().decode([Post].self, from: data)
             
-           
+            
             // Using the Dispatch call allows to run they API while continue to use the app
             
             DispatchQueue.main.async {
@@ -39,14 +39,103 @@ class Api {
                 // complete the Post Array
                 completion(posts)
             }
-       
+            
             
             
         }
         .resume()
     }
 }
+
+
+struct MDTotalVaccine: Codable, Hashable {
+    
+    
+    let features: [Features]?
+    
+    
+}
+
+struct Features: Codable, Hashable {
+    
+    let attributes: Attributes?
+}
+
+struct Attributes: Codable, Hashable {
     
     
     
+    let Metric: String?
+    let Value: Double?
+    
+    
+}
+
+class TotalVaccineData: ObservableObject {
+    
+    
+    @Published var recentMDVaccineTotals: MDTotalVaccine?
+    
+    
+    init() {
+        
+        
+        let url = URL(string: "https://services.arcgis.com/njFNhDsUCentVYJW/arcgis/rest/services/MD_COVID19_VaccinationMetrics/FeatureServer/0/query?where=1%3D1&outFields=Metric,Value&outSR=4326&f=json")!
+        
+        
+        
+        let task =  URLSession.shared.dataTask(with: url) { data, response, error in
+            
+            
+            // ensure there is no error for this HTTP response
+            guard error == nil else {
+                print ("error: \(error!)")
+                return
+            }
+            
+            // ensure there is data returned from this HTTP response
+            guard let data = data else {
+                print("No data")
+                return
+            }
+            
+            // Parse JSON into newJSON struct using JSONDecoder
+            //            guard let allVaccineJSON = try? JSONDecoder().decode(MDTotalVaccine?.self, from: data) else {
+            //               print("Error: Couldn't decode data into all data in MD Info")
+            //               return
+            //             }
+            ////
+            //            print("gotten json response dictionary is \n \(allVaccineJSON)")
+            // update UI using the response here
+            
+            DispatchQueue.main.async {
+                
+                do {
+                    
+                    self.recentMDVaccineTotals = try JSONDecoder().decode(MDTotalVaccine?.self, from: data)
+                    
+                    
+                    
+                    
+                    print(self.recentMDVaccineTotals?.features?[0].attributes?.Metric ?? "No Reponse")
+                    
+                    
+                } catch let jsonError {
+                    
+                    print("Decoding failed for Vaccine Data", jsonError)
+                }
+            }
+            
+            
+        }
+        
+        
+        // execute the HTTP request
+        task.resume()
+        
+    }
+}
+
+
+
 
