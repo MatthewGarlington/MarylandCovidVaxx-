951043036c2acbe403533b5d36e35183e61e0bc2
Matthew Garlington <SBMatthewG@gmail.com>
2021-02-18 09:32:32 -0500

    Initial Commit

---
+++ CovidVaccMD/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CovidVaccMD
+//
+//  Created by Matthew Garlington on 2/18/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CovidVaccMD/CovidVaccMDApp.swift
@@ -0,0 +1,17 @@
+//
+//  CovidVaccMDApp.swift
+//  CovidVaccMD
+//
+//  Created by Matthew Garlington on 2/18/21.
+//
+
+import SwiftUI
+
+@main
+struct CovidVaccMDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
