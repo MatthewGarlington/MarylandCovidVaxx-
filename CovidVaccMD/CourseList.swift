d89bc08a6be8017ff2ebac398029f9a32f12e8f7
Matthew Garlington <SBMatthewG@gmail.com>
2021-02-19 10:44:01 -0500

    feat: Deleted Text to display no text while the Text Data from the CourseDetail overlay loads

--- CovidVaccMD/CourseList.swift
+++ CovidVaccMD/CourseList.swift
@@ -132,15 +132,7 @@ struct CourseView: View {
     var body: some View {
         ZStack(alignment: .top) {
             VStack(alignment: .leading, spacing: 30) {
-                Text("Take your SwiftUI app to the App Store with advanced techniques like API data, packages, and CMS")
-                
-                Text("About this course")
-                    .font(.title)
-                    .bold()
-
-                Text("This course is unlike any other. WE care about design and want to make sure that you get better at it in the process. It was written for designers and developers who are passionate about collaborating and building real apps for ios and MacOS. While it's not one codebase for all apps, you learn once and can apply the techniques and controls to all platforms with incredible consistency and performance. It's a beginner-friendly, but it's also packed with design tricks and efficient workflows for building great user interfaces and interactions")
-
-                Text("Minimal coding experience required, such as in HTML and CSS. Please note that Xcode 11 and Catalina are essential. Once you get everything installed, it'll get a lot friendlier! I added a bunch of troubleshoots at the end of this page to help you navigate the issues you might encounter.")
+                Text("")
                 
             }
             .animation(nil)
@@ -160,6 +152,7 @@ struct CourseView: View {
                             .font(.system(size: 24, weight: .bold))
                             .foregroundColor(Color.white)
                         Text(course.subtitle)
+                            .font(.system(size: 24, weight: .bold))
                             .foregroundColor(Color.white.opacity(0.7))
                     }
                     Spacer()
