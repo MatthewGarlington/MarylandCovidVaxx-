41f5c1328aeda0b458529b87191b04aa9ac186a4
Matthew Garlington <SBMatthewG@gmail.com>
2021-02-19 11:54:39 -0500

    init commit

---
+++ CovidVaccMD/MapView.swift
@@ -0,0 +1,8 @@
+//
+//  MapView.swift
+//  CovidVaccMD
+//
+//  Created by Matthew Garlington on 2/19/21.
+//
+
+import Foundation
---
+++ CovidVaccMD/VaccineMapView.swift
@@ -0,0 +1,20 @@
+//
+//  VaccineMapView.swift
+//  CovidVaccMD
+//
+//  Created by Matthew Garlington on 2/19/21.
+//
+
+import SwiftUI
+
+struct VaccineMapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct VaccineMapView_Previews: PreviewProvider {
+    static var previews: some View {
+        VaccineMapView()
+    }
+}
