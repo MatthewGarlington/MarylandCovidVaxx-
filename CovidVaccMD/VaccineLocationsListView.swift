46ef4eca2f413d639cbba0576587fda6b0fabceb
Matthew Garlington <SBMatthewG@gmail.com>
2021-02-20 10:49:38 -0500

    feat: Added Vaccination Location List to Provide Location Detials for each Vaccination site
    Added this view to give more context on vaccine locations. The View the List items show Maryland Vaccine Location Names, Address, and contact number (if availible) for each location fetched from the Maryland Government Open Data Source on Covid Vaccination Sites

--- CovidVaccMD/VaccineLocationsListView.swift
+++ CovidVaccMD/VaccineLocationsListView.swift
@@ -8,10 +8,64 @@
 import SwiftUI
 
 struct VaccineLocationsListView: View {
+    @ObservedObject var vaccineLocations = VaccineLocationsModel()
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+        List {
+            ForEach(vaccineLocations.vaccineLocationsMD?.features ?? [], id: \.self) { vaccine in
+                NavigationLink(destination: VaccineLocationsDetails(vaccine: vaccine)) {
+                    
+                    HStack {
+                        
+//                        Image(vaccine)
+//                            .resizable()
+//                            .aspectRatio(contentMode: .fit)
+//                            .frame(width: 80, height: 80)
+//                            .background(Color.black)
+//                            .cornerRadius(20)
+//                            .padding(.trailing, 4)
+                        
+            
+                        VStack(alignment: .leading, spacing: 12) {
+           
+                            Text("\(vaccine.attributes?.name ?? "")")
+                                .font(.system(size: 20, weight: .bold))
+                                
+                            Text("\(vaccine.attributes?.fulladdr ?? "")")
+                                .lineLimit(2)
+                                .font(.subheadline)
+                            
+            
+                            
+                            Text("\(vaccine.attributes?.operationalhours ?? "")")
+                                .font(.caption)
+                                .fontWeight(.bold)
+                                .foregroundColor(.secondary)
+                            
+                  
+                        
+                            
+                            
+                            
+                      
+                                
+                            
+                        }
+                    }
+                    .padding(.vertical, 8)
+                }
+            }
+        }
     }
 }
+}
+
+let vaccineData = VaccineLocationsFeatures(attributes: VaccineLocationAttributes.init(name: "", fulladdr: "", municipality: "", ActiveYesNo: "", operationalhours: "", cost_notes: "", schedule_url: "", online_scheduling: "", scheduling_contact: "", scheduling_contact_phone: "", scheduling_contact_email: "", test_pcr: "", website_url: "", created_date: 1, last_edited_date: 10-10-1000), geometry: Geometry(x: 100, y: 100))
+    
+
+
+
+
 
 struct VaccineLocationsListView_Previews: PreviewProvider {
     static var previews: some View {
