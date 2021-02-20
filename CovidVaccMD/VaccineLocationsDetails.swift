9b3e0a7e28b4dea6a59dad6368dcfd051f2142bd
Matthew Garlington <SBMatthewG@gmail.com>
2021-02-20 08:20:19 -0500

    init commit

---
+++ CovidVaccMD/VaccineLocationsDetails.swift
@@ -0,0 +1,20 @@
+//
+//  VaccineLocationsDetails.swift
+//  CovidVaccMD
+//
+//  Created by Matthew Garlington on 2/20/21.
+//
+
+import SwiftUI
+
+struct VaccineLocationsDetails: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct VaccineLocationsDetails_Previews: PreviewProvider {
+    static var previews: some View {
+        VaccineLocationsDetails()
+    }
+}
---
+++ CovidVaccMD/VaccineLocationsListView.swift
@@ -0,0 +1,20 @@
+//
+//  VaccineLocationsListView.swift
+//  CovidVaccMD
+//
+//  Created by Matthew Garlington on 2/20/21.
+//
+
+import SwiftUI
+
+struct VaccineLocationsListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct VaccineLocationsListView_Previews: PreviewProvider {
+    static var previews: some View {
+        VaccineLocationsListView()
+    }
+}
