8f8b138348708cdff9fe09298af81897e6c19772
Matthew Garlington <SBMatthewG@gmail.com>
2021-02-21 08:15:09 -0500

    feat: Deleted all Code in this View as it is no longer needed for reference

--- CovidVaccMD/UpdateDetail.swift
+++ CovidVaccMD/UpdateDetail.swift
@@ -5,31 +5,31 @@
 //  Created by Matthew Garlington on 2/18/21.
 //
 
-import SwiftUI
-
-
-struct UpdateDetail: View {
-    
-    var update: Update = updateData[0]
-    
-    var body: some View {
-        ScrollView {
-            VStack {
-                Image(update.image)
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(maxWidth: .infinity)
-                Text(update.text)
-                    .frame(maxWidth: .infinity)
-            }
-            .navigationBarTitle(update.title)
-        }
-        .listStyle(PlainListStyle())
-    }
-}
-
-struct UpdateDetail_Previews: PreviewProvider {
-    static var previews: some View {
-        UpdateDetail()
-    }
-}
+//import SwiftUI
+//
+//
+//struct UpdateDetail: View {
+//    
+//    var update: Update = updateData[0]
+//    
+//    var body: some View {
+//        ScrollView {
+//            VStack {
+//                Image(update.image)
+//                    .resizable()
+//                    .aspectRatio(contentMode: .fit)
+//                    .frame(maxWidth: .infinity)
+//                Text(update.text)
+//                    .frame(maxWidth: .infinity)
+//            }
+//            .navigationBarTitle(update.title)
+//        }
+//        .listStyle(PlainListStyle())
+//    }
+//}
+//
+//struct UpdateDetail_Previews: PreviewProvider {
+//    static var previews: some View {
+//        UpdateDetail()
+//    }
+//}
