39ff6113ee5aece1759638dc2795077a8d25366b
Matthew Garlington <SBMatthewG@gmail.com>
2021-02-21 16:41:19 -0500

    feat: Added A Class For CovidMasterModel that calls data from the Maryland Open Source Data for All Covid Data
    This was added to provide detailed data for Deaths and Cases broken down by County, Gender, Age, and Race

--- CovidVaccMD/ViewModel/Data.swift
+++ CovidVaccMD/ViewModel/Data.swift
@@ -43,6 +43,194 @@ class Api {
 }
 
 
+struct MDHeader: Codable, Hashable {
+    
+    let filter: String?
+    let reportdate: String?
+    let totalcases: String?
+    let casedelta: String?
+    let totaltests: String?
+    let testsdelta: String?
+    let postestpercent: String?
+    let pospercentdiff: String?
+    let negativetests: String?
+    let negdelta: String?
+    let bedstotal: String?
+    let bedsacute: String?
+    let bedsicu: String?
+    let bedsdelta: String?
+    let total_hospitalized: String?
+    let hospitalizeddelta: String?
+    let total_released: String?
+    let releaseddelta: String?
+    
+}
+
+struct MDCases: Codable, Hashable {
+  
+    let alle: String?
+    let anne: String?
+    let balt: String?
+    let bcity: String?
+    let calv: String?
+    let caro: String?
+    let carr: String?
+    let ceci: String?
+    let char: String?
+    let dorc: String?
+    let fred: String?
+    let garr: String?
+    let harf: String?
+    let howa: String?
+    let kent: String?
+    let mont: String?
+    let prin: String?
+    let quee: String?
+    let some: String?
+    let stma: String?
+    let talb: String?
+    let wash: String?
+    let wico: String?
+    let worc: String?
+    let case0to9: String?
+    let case10to19: String?
+    let case20to29: String?
+    let case30to39: String?
+    let case40to49: String?
+    let case50to59: String?
+    let case60to69: String?
+    let case70to79: String?
+    let case80plus: String?
+    let genmale: String?
+    let genfemale: String?
+    let caseaframer: String?
+    let casewhite: String?
+    let casehispanic: String?
+    let caseasian: String?
+    let caseother: String?
+    
+    
+}
+
+struct MDDataDeath: Codable, Hashable {
+    let filter: String?
+    let deathsdelta: String?
+    let deaths: String?
+    let deathdod: String?
+    let deathalle: String?
+    let deathanne: String?
+    let deathbalt: String?
+    let deathbcity: String?
+    let deathcalv: String?
+    let deathcaro: String?
+    let deathcarr: String?
+    let deathceci: String?
+    let deathchar: String?
+    let deathdorc: String?
+    let deathfred: String?
+    let deathgarr: String?
+    let deathharf: String?
+    let deathhowa: String?
+    let deathkent: String?
+    let deathmont: String?
+    let deathprin: String?
+    let deathquee: String?
+    let deathsome: String?
+    let deathstma: String?
+    let deathtalb: String?
+    let deathwash: String?
+    let deathwico: String?
+    let deathworc: String?
+    let deathunkn: String?
+    let death0to9: String?
+    let death10to19: String?
+    let death20to29: String?
+    let death30to39: String?
+    let death40to49: String?
+    let death50to59: String?
+    let death60to69: String?
+    let death70to79: String?
+    let death80plus: String?
+    let deathgenmale: String?
+    let deathgenfemale: String?
+    let deathaframer: String?
+    let deathwhite: String?
+    let deathhispanic: String?
+    let deathasian: String?
+    let deathother: String?
+    let deathnotavail: String?
+    
+}
+
+class CovidMasterModel: ObservableObject {
+    
+    
+    @Published var allMDHeader = [MDHeader]()
+    @Published var allDeaths = [MDDataDeath]()
+    @Published var allMDCases = [MDCases]()
+    
+    
+    
+    
+    init() {
+        
+        
+        
+        let url = URL(string: "https://opendata.maryland.gov/resource/mgd3-qk8t.json")!
+        
+        
+        let task =  URLSession.shared.dataTask(with: url) { data, response, error in
+            
+            
+            
+            // ensure there is no error for this HTTP response
+            guard error == nil else {
+                print ("error: \(error!)")
+                return
+            }
+            
+            // ensure there is data returned from this HTTP response
+            guard let data = data else {
+                print("No data")
+                return
+            }
+            
+            // Parse JSON into newJSON struct using JSONDecoder
+//                        guard let locationVaccine = try? JSONDecoder().decode([MDHeader].self, from: data) else {
+//                           print("Error: Couldn't decode data into all data in MD Info")
+//                           return
+//                         }
+//            //
+//                        print("gotten json response dictionary is \n \(locationVaccine)")
+            // update UI using the response here
+            
+            DispatchQueue.main.async {
+
+                do {
+
+                    self.allMDHeader = try JSONDecoder().decode([MDHeader].self, from: data)
+                    self.allMDCases = try JSONDecoder().decode([MDCases].self, from: data)
+                    self.allDeaths = try JSONDecoder().decode([MDDataDeath].self, from: data)
+     
+                    print(self.allMDHeader[self.allMDHeader.count - 1].bedstotal ?? "")
+
+                } catch let jsonError {
+
+                    print("Decoding failed for All Maryland Covid Data", jsonError)
+                }
+            }
+            
+            
+        }
+        
+        
+        // execute the HTTP request
+        task.resume()
+        
+    }
+}
+
+
 
 
 
@@ -93,10 +281,7 @@ class VaccineLocationsModel: ObservableObject {
                     
                     self.vaccineLocationsMD = try JSONDecoder().decode(MDVaccineLocations?.self, from: data)
                     
-                    
-                    
-                    
-                    print(self.vaccineLocationsMD?.features?[0].geometry?.coordinate)
+      
                     
                     
                 } catch let jsonError {
