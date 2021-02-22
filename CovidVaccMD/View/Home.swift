049439c4f5420110c788ad844e05565cedac045e
Matthew Garlington <SBMatthewG@gmail.com>
2021-02-22 09:47:29 -0500

    testing: Added MDTotalConfirmedDeathsViewModel to call upon launch of app to display print data from Data to debugger

--- CovidVaccMD/View/Home.swift
+++ CovidVaccMD/View/Home.swift
@@ -13,7 +13,7 @@ struct Home: View {
     @State var showContent = false
     @State var showList = false
     @EnvironmentObject var user: UserStore
-    @ObservedObject var positiveCasesViewModel = PositiveCasesViewModel()
+   
     
 
     
