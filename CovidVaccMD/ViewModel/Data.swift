705cabb7939a3de3ffd5290f422867658353c093
Matthew Garlington <SBMatthewG@gmail.com>
2021-02-22 09:05:02 -0500

    feat: Added an API Call to the Maryland State Gov's Open Data Covid Information in order to display specfic data for Percent Positive in given form of Double
    This was added in order to give a double to the RingView to show an updated graph of the amount of the population testing positive for Covid 19 in the state of Maryland

--- CovidVaccMD/ViewModel/Data.swift
+++ CovidVaccMD/ViewModel/Data.swift
@@ -162,6 +162,119 @@ struct MDDataDeath: Codable, Hashable {
     
 }
 
+
+
+
+struct MDPositiveCases: Codable, Hashable {
+    
+    
+    let features: [CaseFeatures]?
+    
+    
+}
+
+struct CaseFeatures: Codable, Hashable {
+    
+    
+    let attributes: CaseAttributes?
+}
+
+struct CaseAttributes: Codable, Hashable {
+    
+    
+    
+    let date: Int?
+    let number_of_tests: Int?
+    let number_of_positives: Int?
+    let percent_positive: Double?
+    let rolling_avg: Double?
+    
+    
+}
+
+
+
+class PositiveCasesViewModel: ObservableObject {
+    
+    
+    @Published var positiveCases: MDPositiveCases?
+  //  @Published var caseRing: [CaseRing] = []
+    
+    
+    
+    
+    init() {
+        
+        
+        
+        let url = URL(string: "https://services.arcgis.com/njFNhDsUCentVYJW/arcgis/rest/services/MDCOVID19_TestingVolume/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json")!
+        
+        
+        let task =  URLSession.shared.dataTask(with: url) { data, response, error in
+            
+            
+            
+            // ensure there is no error for this HTTP response
+            guard error == nil else {
+                print ("error: \(error!)")
+                return
+            }
+            
+            // ensure there is data returned from this HTTP response
+            guard let data = data else {
+                print("No data")
+                return
+            }
+            
+            // Parse JSON into newJSON struct using JSONDecoder
+//                        guard let positiveCases = try? JSONDecoder().decode(MDPositiveCases?.self, from: data) else {
+//                           print("Error: Couldn't decode data into all data in MD Info")
+//                           return
+//                         }
+//            //
+//                        print("gotten json response dictionary is \n \(positiveCases)")
+            // update UI using the response here
+//
+            DispatchQueue.main.async {
+
+                do {
+                    
+                    
+
+                    self.positiveCases = try JSONDecoder().decode(MDPositiveCases?.self, from: data)
+                    
+                    let epochTime2 = TimeInterval((self.positiveCases?.features?[(self.positiveCases?.features!.count)! - 1].attributes?.date ?? 0)) / 1000
+                    let date2 = Date(timeIntervalSince1970: epochTime2)
+                    let formate = date2.getFormattedDate(format: "MM-dd-yyyy")
+                    
+
+                   // print(formate)
+
+                } catch let jsonError {
+
+                    print("Decoding failed for UserDetails", jsonError)
+                }
+            }
+            
+            
+        }
+        
+        
+        // execute the HTTP request
+        task.resume()
+        
+    }
+}
+
+extension Date {
+   func getFormattedDate(format: String) -> String {
+        let dateformat = DateFormatter()
+        dateformat.dateFormat = format
+        return dateformat.string(from: self)
+    }
+}
+
+
 class CovidMasterModel: ObservableObject {
     
     
