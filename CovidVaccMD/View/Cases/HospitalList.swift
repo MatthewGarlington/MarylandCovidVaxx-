99db280160e882f7fa3f8a76bf1d8b78eeb09f03
Matthew Garlington <SBMatthewG@gmail.com>
2021-02-23 09:01:13 -0500

    feat: Added Lsit view to present Hospital data in a historical context broken down by each day since the data was taken
    The list view added has a list item  title of the current date for the information followed by total hospitalized with change in the past 24 hours added to that HStack, Total in ICU, and Total in Acute care for that given day. The data is taken from the Maryland State gov Open Data API for Covid 19 and called from the Observed object of CovidMasterModel class.

--- CovidVaccMD/View/Cases/HospitalList.swift
+++ CovidVaccMD/View/Cases/HospitalList.swift
@@ -8,13 +8,112 @@
 import SwiftUI
 
 struct HospitalList: View {
+    @ObservedObject var casesModel = CovidMasterModel()
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+ 
+        NavigationView {
+        List {
+            ForEach(casesModel.allMDHeader.reversed(), id: \.self) { cases in
+       
+                    
+                    HStack {
+
+                        
+            
+                        VStack(alignment: .leading, spacing: 12) {
+                           
+                            
+                            VStack(alignment: .leading, spacing: 10) {
+
+
+
+
+                                Text("\((cases.reportdate?.getDateFormatterForString(dateString: cases.reportdate ?? ""))!):")
+                                    .font(.system(size: 20, weight: .bold))
+
+
+                          
+
+                                VStack(alignment: .leading) {
+                                    HStack(spacing: 10) {
+                                        HStack {
+                                            
+                                            Text("Total Hospitalized:")
+                                                .bold()
+                                            
+                                            Text("\(cases.total_hospitalized ?? "")")
+                                                .font(.system(size: 20, weight: .semibold))
+                                        }
+
+
+
+                                    VStack(alignment: .leading) {
+                                        Text("+\(cases.hospitalizeddelta ?? "")")
+                                            .font(.caption)
+                                            .fontWeight(.bold)
+                                            .foregroundColor(.secondary)
+                                        Spacer()
+                                    }
+                                    }
+                                    
+                                    HStack(spacing: 10) {
+                                        HStack {
+                                            
+                                            Text("Total in ICU:")
+                                                .bold()
+                                            
+                                            Text("\(cases.bedsicu ?? "")")
+                                                .font(.system(size: 20, weight: .semibold))
+                                        }
+
+
+
+                                    }
+                                    
+                                    HStack(spacing: 10) {
+                                        HStack {
+                                            
+                                            Text("Total in Acute Care:")
+                                                .bold()
+                                            
+                                            Text("\(cases.bedsacute ?? "")")
+                                                .font(.system(size: 20, weight: .semibold))
+                                        }
+
+
+
+                                    }
+                                }
+                            }
+                          
+                            
+                            
+                        
+ 
+                            
+                        }
+                    }
+                    .padding(.vertical, 8)
+                
+            }
+            .navigationBarTitle("History of Total Case numbers")
+            .navigationBarTitleDisplayMode(.inline)
+        }
+       
+    
+      
+        }
+    
 }
+}
+
 
 struct HospitalList_Previews: PreviewProvider {
     static var previews: some View {
         HospitalList()
     }
 }
+
+
+
