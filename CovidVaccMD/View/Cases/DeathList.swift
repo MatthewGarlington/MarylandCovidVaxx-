dcc9829249b6a323d1e63fa3edf9a99092005367
Matthew Garlington <SBMatthewG@gmail.com>
2021-02-23 11:42:24 -0500

    feat: Added List View to show the data of all historical deaths from covid 19 as well as 24 hour change broken down by each day since the data was recorded

--- CovidVaccMD/View/Cases/DeathList.swift
+++ CovidVaccMD/View/Cases/DeathList.swift
@@ -8,11 +8,107 @@
 import SwiftUI
 
 struct DeathList: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
+    @ObservedObject var casesModel = CovidMasterModel()
 
+    var body: some View {
+        NavigationView {
+        List {
+            ForEach(casesModel.allDeaths.reversed(), id: \.self) { deaths in
+       
+                    
+                    HStack {
+
+                        
+            
+                        VStack(alignment: .leading, spacing: 12) {
+                           
+                            
+                            VStack(alignment: .leading, spacing: 10) {
+
+
+
+
+                                Text("\((deaths.reportdate?.getDateFormatterForString(dateString: deaths.reportdate ?? ""))!):")
+                                    .font(.system(size: 20, weight: .bold))
+
+
+                          
+
+                                VStack(alignment: .leading) {
+                                    HStack(spacing: 10) {
+                                        HStack {
+                                            
+                                            Text("Total Deaths:")
+                                                .bold()
+                                            
+                                            Text("\(deaths.deaths ?? "")")
+                                                .font(.system(size: 20, weight: .semibold))
+                                        }
+
+
+
+                                    VStack(alignment: .leading) {
+                                        Text("+\(deaths.deathsdelta ?? "")")
+                                            .font(.caption)
+                                            .fontWeight(.bold)
+                                            .foregroundColor(.secondary)
+                                        Spacer()
+                                    }
+                                    }
+                                    
+//                                    HStack(spacing: 10) {
+//                                        HStack {
+//
+//                                            Text("Total in ICU:")
+//                                                .bold()
+//
+//                                            Text("\(cases.bedsicu ?? "")")
+//                                                .font(.system(size: 20, weight: .semibold))
+//                                        }
+//
+//
+//
+//                                    }
+//
+//                                    HStack(spacing: 10) {
+//                                        HStack {
+//
+//                                            Text("Total in Acute Care:")
+//                                                .bold()
+//
+//                                            Text("\(cases.bedsacute ?? "")")
+//                                                .font(.system(size: 20, weight: .semibold))
+//                                        }
+//
+//
+//
+//                                    }
+                                }
+                            }
+                          
+                            
+                            
+                        
+ 
+                            
+                        }
+                    }
+                    .padding(.vertical, 8)
+                
+            }
+            .navigationBarTitle("History of Total Deaths")
+            .navigationBarTitleDisplayMode(.inline)
+        }
+       
+    
+      
+        }
+    
+
+
+    
+}
+}
 struct DeathList_Previews: PreviewProvider {
     static var previews: some View {
         DeathList()
