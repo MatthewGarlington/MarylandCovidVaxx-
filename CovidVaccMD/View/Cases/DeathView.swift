dc1fc5a9f2037608f888f2e9d1556ae2e46fe1d6
Matthew Garlington <SBMatthewG@gmail.com>
2021-02-23 11:40:42 -0500

    feat: Added Button to HStack of card in DeathView to show DeathList Upon press
    This button was added to give the user the ability to see the history of all deaths recorded broken down by date and new deaths within the last 24 hours. This data was received from the Maryland State Gov Open Data API

--- CovidVaccMD/View/Cases/DeathView.swift
+++ CovidVaccMD/View/Cases/DeathView.swift
@@ -14,6 +14,7 @@ struct DeathView: View {
     var deaths: DeathRow
     var width: CGFloat = 275
     var height: CGFloat = 275
+    @State var showDeathList = false
   
     
     var body: some View {
@@ -24,20 +25,36 @@ struct DeathView: View {
                     HStack(alignment: .top) {
                         Text(deaths.title)
                             .font(.system(size: 24, weight: .bold))
-                            .frame(width: 225, alignment: .leading)
+                            .frame(width: 200, alignment: .leading)
                             .frame(height: 100)
-                            .padding(.top, 35)
+                            .padding(.top, 40)
                             .foregroundColor(.white)
-                            .padding(.horizontal, 10)
                         Spacer()
-                    //    Image(uiImage: section.logo)
+                        Button(action: {self.showDeathList.toggle()}) {
+                            Image(systemName: "list.bullet.indent")
+                                .foregroundColor(Color.red)
+                                .font(.system(size: 16, weight: .medium))
+                                .frame(width: 36, height: 36)
+                                .background(Color("background3"))
+                                .clipShape(Circle())
+                                .padding(.top, 40)
+                                .shadow(color: Color.black.opacity(0.1), radius: 1, x: 0, y: 1)
+                                .shadow(color: Color.black.opacity(0.2), radius: 10, x: 0, y: 10)
+                            
+                            
+                        }
+                        .sheet(isPresented: $showDeathList) {
+                            
+                            DeathList()
+                      
+                        }
                         
                     }
                   Text(deaths.text)
-                        .font(.system(size: 24, weight: .bold))
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                        .frame(height: 75)
-                        .padding(.horizontal, 10)
+                    .font(.system(size: 24, weight: .bold))
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .frame(height: 50)
+                    .padding(.bottom, 10)
                     
                 }
             
@@ -48,7 +65,7 @@ struct DeathView: View {
                 WebImage(url: deaths.image)
                     .resizable()
                     .aspectRatio(contentMode: .fill)
-                    .frame(height: 150)
+                    .frame(height: 125)
                    //.padding(.bottom, 20)
                    //.blendMode(.darken)
                 }
