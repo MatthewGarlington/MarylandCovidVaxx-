b924e398cc2ca1e3647bb1b79f0d6f6bb058b208
Matthew Garlington <SBMatthewG@gmail.com>
2021-02-24 13:52:39 -0500

    feat: Added UIViewRepresentable type to HospitalMapViewMaryland struct
    This was down to retreive only Hospital Vaccine Location Data from the Maryland Gov Open Data. By using a for in loop and only appending pinsHospitalArray, this will present only the specific Hospital Vaccine location data

--- CovidVaccMD/View/Home/MapView.swift
+++ CovidVaccMD/View/Home/MapView.swift
@@ -217,3 +217,126 @@ struct MapViewMaryland_Previews: PreviewProvider {
         MapViewMaryland(annotations: .constant(MKPointAnnotation.exampleArray), pinsLocalHealthArray: .constant(MKPointAnnotation.exampleArray), pinsHospitalArray: .constant(MKPointAnnotation.exampleArray), pinsMassArray: .constant(MKPointAnnotation.exampleArray), pinsRetailArray: .constant(MKPointAnnotation.exampleArray), selectedPlace: .constant(MKPointAnnotation.example), showingPlaceDetails:  .constant(true))
     }
 }
+
+
+
+struct HospitalMapViewMaryland: UIViewRepresentable {
+    
+
+    @ObservedObject var hospitalVaccinationsViewModel = HospitalVaccineLocationsModel()
+    @Binding var annotations: [MKPointAnnotation]
+    @Binding var pinsHospitalArray: [MKPointAnnotation]
+    @Binding var selectedPlace: MKPointAnnotation?
+    @Binding var showingPlaceDetails: Bool
+
+  
+
+    func makeUIView(context: Context) -> MKMapView {
+        let mapView = MKMapView()
+        let region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 39.045753, longitude: -76.641273), latitudinalMeters: CLLocationDistance(exactly: 300000)!, longitudinalMeters: CLLocationDistance(exactly: 300000)!)
+        mapView.setRegion(mapView.regionThatFits(region), animated: true)
+        mapView.delegate = context.coordinator
+        
+
+     return mapView
+    }
+
+    func updateUIView(_ view: MKMapView, context: Context) {
+
+   
+        
+        for i in stride(from: 0, through: (((hospitalVaccinationsViewModel.vaccineLocationsMD?.features?.count ?? 0)-1)), by: 1)
+        {
+            
+            
+        
+        let pinsHospitalArray = MKPointAnnotation()
+            
+           
+
+
+            pinsHospitalArray.title = hospitalVaccinationsViewModel.vaccineLocationsMD?.features?[i].attributes?.name ?? ""
+            pinsHospitalArray.coordinate = CLLocationCoordinate2D(latitude:(hospitalVaccinationsViewModel.vaccineLocationsMD?.features?[i].geometry?.coordinate.latitude ?? 0.0), longitude: (hospitalVaccinationsViewModel.vaccineLocationsMD?.features?[i].geometry?.coordinate.longitude ?? 0.0))
+            pinsHospitalArray.subtitle = hospitalVaccinationsViewModel.vaccineLocationsMD?.features?[i].attributes?.fulladdr ?? ""
+        
+     
+            view.addAnnotation(pinsHospitalArray)
+              
+            
+        }
+        
+             
+     
+                
+
+    }
+    
+    
+ 
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+
+    class Coordinator: NSObject, MKMapViewDelegate {
+        var parent: HospitalMapViewMaryland
+
+        init(_ parent: HospitalMapViewMaryland) {
+            self.parent = parent
+            
+            
+        }
+        func mapViewDidChangeVisibleRegion(_ view: MKMapView) {
+        //    parent.centerCoordinate = view.centerCoordinate
+          //  parent.region = view.region
+             
+            
+          
+         
+        }
+        
+  
+        func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+            if !(annotation is MKUserLocation) {
+                let pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: String(annotation.hash))
+
+                let rightButton = UIButton(type: .detailDisclosure)
+                pinView.rightCalloutAccessoryView = rightButton
+                pinView.animatesDrop = false
+                pinView.canShowCallout = true
+             
+              
+             
+               
+                
+                
+
+                return pinView
+            }
+            else {
+                return nil
+            }
+        }
+        func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+            
+            guard let placemark = view.annotation as? MKPointAnnotation? else { return }
+            
+
+            parent.selectedPlace = placemark
+            parent.showingPlaceDetails = true
+        }
+        
+        
+    }
+    
+    
+}
+
+
+
+struct HospitalMapViewMaryland_Previews: PreviewProvider {
+    static var previews: some View {
+        HospitalMapViewMaryland(annotations: .constant(MKPointAnnotation.exampleArray), pinsHospitalArray: .constant(MKPointAnnotation.exampleArray), selectedPlace: .constant(MKPointAnnotation.example), showingPlaceDetails: .constant(true))
+    }
+}
+
