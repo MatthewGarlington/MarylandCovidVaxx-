fdd69961903fcd1e38b33b975010c33e0362d73e
Matthew Garlington <SBMatthewG@gmail.com>
2021-02-24 18:45:02 -0500

    refactor: Added Map Folder to Store all map related data

---
+++ CovidVaccMD/View/Map/MapTabView.swift
@@ -0,0 +1,20 @@
+//
+//  MapTabView.swift
+//  CovidVaccMD
+//
+//  Created by Matthew Garlington on 2/24/21.
+//
+
+import SwiftUI
+
+struct MapTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MapTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapTabView()
+    }
+}
