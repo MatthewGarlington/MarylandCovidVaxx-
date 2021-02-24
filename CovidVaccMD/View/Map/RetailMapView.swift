9ecffe22cd5f7b5cd245f25e81675146fdbcae71
Matthew Garlington <SBMatthewG@gmail.com>
2021-02-24 18:50:19 -0500

    feat: Added Title card on Top of Map to Display the title
    Added to Title to give contents when this view is called of what dats ais being shown on the Map

--- CovidVaccMD/View/Map/RetailMapView.swift
+++ CovidVaccMD/View/Map/RetailMapView.swift
@@ -15,16 +15,9 @@ struct RetailMapView: View {
     @State var selectedPlace: MKPointAnnotation?
     var body: some View {
         
-   ZStack(alignment: .bottom) {
+   ZStack(alignment: .top) {
 
-       
-       VStack {
-           
-           Spacer()
-                   
-           VStack {
-     
-                   Spacer()
+
                      
             RetailMapViewMaryland(annotations: $annotations, pinsRetailArray: $pinsRetailArray, selectedPlace: $selectedPlace, showingPlaceDetails: $showingPlaceDetails)
 
@@ -33,11 +26,37 @@ struct RetailMapView: View {
                
                    Alert(title: Text(selectedPlace?.title ?? ""), message: Text(selectedPlace?.subtitle ?? ""))
                    }
+    
+    ZStack {
+        
+        BlurView(style: .systemThickMaterial)
+            .frame(maxWidth: .infinity)
+            .cornerRadius(15)
+            .frame(height: 75)
+            .shadow(color: Color.black.opacity(0.2), radius: 10, x: 0, y: 12)
+            .shadow(color: Color.black.opacity(0.1), radius: 1, x: 0, y: 1)
+            .padding()
+ 
+        
+        
+        
+        
+        
+        VStack {
+           
+            Text("Retail Store")
+                .bold()
+            
+            
+            Text("Vaccination Locations")
+                .bold()
+           
+                
+        }
+    }
            }
        }
    }
-    }
-}
 
 struct RetailMapView_Previews: PreviewProvider {
     static var previews: some View {
