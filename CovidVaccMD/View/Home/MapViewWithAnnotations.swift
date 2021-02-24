b4a6113b6f1b22a8bfeeeb8197f1ff6450eb9791
Matthew Garlington <SBMatthewG@gmail.com>
2021-02-24 10:16:31 -0500

    feat: Added Class of MassVaccinationViewModel to fetch data from Data Base For MD Vaccination ArcGIS Online for Maryland
    This was added to display Vaccination Locations for Covid 19 in the State of Maryland with Data from the Maryland State Gov in order to show mass vaccination locations

---
+++ CovidVaccMD/View/Home/MapViewWithAnnotations.swift
@@ -0,0 +1,20 @@
+//
+//  MapViewWithAnnotations.swift
+//  CovidVaccMD
+//
+//  Created by Matthew Garlington on 2/24/21.
+//
+
+import SwiftUI
+
+struct MapViewWithAnnotations: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MapViewWithAnnotations_Previews: PreviewProvider {
+    static var previews: some View {
+        MapViewWithAnnotations()
+    }
+}
