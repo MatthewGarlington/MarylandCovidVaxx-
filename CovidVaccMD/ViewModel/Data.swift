68a28417702f62f48c38b97656c7ecc70b5f01cc
Matthew Garlington <SBMatthewG@gmail.com>
2021-02-24 12:33:28 -0500

    feat: added HospitalVaccineLocationsModel view model to connect to Data Base For MD Vaccination ArcGIS Online for Maryland
    Added this feature to connect to Maryland State Gov Open Data base to show Vaccine location information for Covid 19 Vaccine Locations located in Hospitals in the State of Maryland

--- CovidVaccMD/ViewModel/Data.swift
+++ CovidVaccMD/ViewModel/Data.swift
@@ -364,7 +364,76 @@ class CovidMasterModel: ObservableObject {
 
 
 
-class VaccineLocationsModel: ObservableObject {
+class LocalHealthVaccineLocationsModel: ObservableObject {
+    
+    
+    @Published var vaccineLocationsMD: MDVaccineLocations?
+    
+    
+    
+    
+    init() {
+        
+        
+        
+        let url = URL(string: "https://services.arcgis.com/njFNhDsUCentVYJW/arcgis/rest/services/MD_Vaccination_Locations/FeatureServer/2/query?where=1%3D1&outFields=*&outSR=4326&f=json")!
+        
+        
+        let task =  URLSession.shared.dataTask(with: url) { data, response, error in
+            
+            
+            
+            // ensure there is no error for this HTTP response
+            guard error == nil else {
+                print ("error: \(error!)")
+                return
+            }
+            
+            // ensure there is data returned from this HTTP response
+            guard let data = data else {
+                print("No data")
+                return
+            }
+            
+            // Parse JSON into newJSON struct using JSONDecoder
+            //            guard let locationVaccine = try? JSONDecoder().decode(MDVaccineLocations.self, from: data) else {
+            //               print("Error: Couldn't decode data into all data in MD Info")
+            //               return
+            //             }
+            ////
+            //            print("gotten json response dictionary is \n \(locationVaccine)")
+            // update UI using the response here
+            
+            DispatchQueue.main.async {
+                
+                do {
+                    
+                    self.vaccineLocationsMD = try JSONDecoder().decode(MDVaccineLocations?.self, from: data)
+                    
+      
+                    
+                    
+                    
+                    
+                    
+                    
+                } catch let jsonError {
+                    
+                    print("Decoding failed for UserDetails", jsonError)
+                }
+            }
+            
+            
+        }
+        
+        
+        // execute the HTTP request
+        task.resume()
+        
+    }
+}
+
+class HospitalVaccineLocationsModel: ObservableObject {
     
     
     @Published var vaccineLocationsMD: MDVaccineLocations?
