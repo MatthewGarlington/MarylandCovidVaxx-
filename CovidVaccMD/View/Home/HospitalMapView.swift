0be4f3c72b261cd9cec5f7295c7768f7d9f196a4
Matthew Garlington <SBMatthewG@gmail.com>
2021-02-24 13:50:00 -0500

    feat: Added seperate mapview for Hospital Pins and Annotations only
    This was added in order to display a map view using HospitalMapViewMaryland that shows only Hospital Vaccine Location Data

---
+++ CovidVaccMD/View/Home/HospitalMapView.swift
@@ -0,0 +1,20 @@
+//
+//  HospitalMapView.swift
+//  CovidVaccMD
+//
+//  Created by Matthew Garlington on 2/24/21.
+//
+
+import SwiftUI
+
+struct HospitalMapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HospitalMapView_Previews: PreviewProvider {
+    static var previews: some View {
+        HospitalMapView()
+    }
+}
--- CovidVaccMD/View/Home/MapViewWithAnnotations.swift
+++ CovidVaccMD/View/Home/MapViewWithAnnotations.swift
@@ -10,41 +10,30 @@ import MapKit
 
 struct MapViewWithAnnotations: View {
     
-    @ObservedObject var localHealthVaccineLocations = LocalHealthVaccineLocationsModel()
-    @ObservedObject var massVaccinationsitesViewModel = MassVaccinationViewModel()
+
     @State var annotations = [MKPointAnnotation]()
     @State var pinsLocalHealthArray = [MKPointAnnotation]()
     @State var pinsMassArray = [MKPointAnnotation]()
     @State var pinsRetailArray = [MKPointAnnotation]()
     @State var pinsHospitalArray = [MKPointAnnotation]()
     @State var showingPlaceDetails = false
-    @State var showingEditScreen = false
     @State var selectedPlace: MKPointAnnotation?
     var body: some View {
         
    ZStack(alignment: .bottom) {
        
-       Color("background2")
-           .edgesIgnoringSafeArea(.all)
-       
+
        VStack {
            
            Spacer()
                    
            VStack {
-               Rectangle()
-                           .frame(width: 75, height: 5)
-                           .cornerRadius(3)
-                           .opacity(0.1)
-                   .padding()
+    
            
                    Spacer()
                      
             MapViewMaryland(annotations: $annotations, pinsLocalHealthArray: $pinsLocalHealthArray, pinsHospitalArray: $pinsHospitalArray, pinsMassArray: $pinsMassArray, pinsRetailArray: $pinsRetailArray, selectedPlace: $selectedPlace, showingPlaceDetails: $showingPlaceDetails)
-                       
-                       .frame(maxWidth: 712)
-                       .frame(height: 750)
-                       .cornerRadius(50)
+        
                        .alert(isPresented: $showingPlaceDetails) {
                
                
