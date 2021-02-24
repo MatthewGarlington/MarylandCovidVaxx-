094b0080aceadfdb1e6660ef8371a7c27892cdc3
Matthew Garlington <SBMatthewG@gmail.com>
2021-02-24 13:55:24 -0500

    feat: Added HospitalMapViewMaryland to display Map Points with annotations for only Hospital locations

--- CovidVaccMD/View/Home/HospitalMapView.swift
+++ CovidVaccMD/View/Home/HospitalMapView.swift
@@ -6,13 +6,38 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct HospitalMapView: View {
+    @State var annotations = [MKPointAnnotation]()
+    @State var pinsHospitalArray = [MKPointAnnotation]()
+    @State var showingPlaceDetails = false
+    @State var selectedPlace: MKPointAnnotation?
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+   ZStack(alignment: .bottom) {
+
+       
+       VStack {
+           
+           Spacer()
+                   
+           VStack {
+     
+                   Spacer()
+                     
+            HospitalMapViewMaryland(annotations: $annotations, pinsHospitalArray: $pinsHospitalArray, selectedPlace: $selectedPlace, showingPlaceDetails: $showingPlaceDetails)
+
+                       .alert(isPresented: $showingPlaceDetails) {
+               
+               
+                   Alert(title: Text(selectedPlace?.title ?? ""), message: Text(selectedPlace?.subtitle ?? ""))
+                   }
+           }
+       }
+   }
     }
 }
-
 struct HospitalMapView_Previews: PreviewProvider {
     static var previews: some View {
         HospitalMapView()
