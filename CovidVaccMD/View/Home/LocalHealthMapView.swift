f90803d47ed803ff81da16d8790bc7009f374f7f
Matthew Garlington <SBMatthewG@gmail.com>
2021-02-24 14:56:33 -0500

    feat: Added View to show Map pins and annotations for Covid 19 Vaccine Location information for only Local Medical Center Vaccination sites in the State of Maryland

--- CovidVaccMD/View/Home/LocalHealthMapView.swift
+++ CovidVaccMD/View/Home/LocalHealthMapView.swift
@@ -6,13 +6,38 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct LocalHealthMapView: View {
+    @State var annotations = [MKPointAnnotation]()
+    @State var pinsLocalHealthArray = [MKPointAnnotation]()
+    @State var showingPlaceDetails = false
+    @State var selectedPlace: MKPointAnnotation?
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+   ZStack(alignment: .bottom) {
+
+       
+       VStack {
+           
+           Spacer()
+                   
+           VStack {
+     
+                   Spacer()
+                     
+            LocalHealthMapViewMaryland(annotations: $annotations, pinsLocalHealthArray: $pinsLocalHealthArray, selectedPlace: $selectedPlace, showingPlaceDetails: $showingPlaceDetails)
+
+                       .alert(isPresented: $showingPlaceDetails) {
+               
+               
+                   Alert(title: Text(selectedPlace?.title ?? ""), message: Text(selectedPlace?.subtitle ?? ""))
+                   }
+           }
+       }
+   }
     }
 }
-
 struct LocalHealthMapView_Previews: PreviewProvider {
     static var previews: some View {
         LocalHealthMapView()
