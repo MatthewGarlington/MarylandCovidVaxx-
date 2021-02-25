151371e74f8799140b14d189ff2a3c1762801480
Matthew Garlington <SBMatthewG@gmail.com>
2021-02-25 11:57:50 -0500

    feat: Added List view for All  Covid 19 Testing locations in the state of Maryland

--- CovidVaccMD/View/TestingList/AllTestingList.swift
+++ CovidVaccMD/View/TestingList/AllTestingList.swift
@@ -8,9 +8,69 @@
 import SwiftUI
 
 struct AllTestingList: View {
+    
+    @ObservedObject var allTestingLocations = AllTestingLocations()
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+        List {
+       
+            ForEach(allTestingLocations.testingTestingLocations?.features ?? [], id: \.self) { testing in
+                NavigationLink(destination: TestingLocationsDetail(testing: testing)) {
+                    
+                    HStack {
+                        
+//                        Image(vaccine)
+//                            .resizable()
+//                            .aspectRatio(contentMode: .fit)
+//                            .frame(width: 80, height: 80)
+//                            .background(Color.black)
+//                            .cornerRadius(20)
+//                            .padding(.trailing, 4)
+                        
+            
+                        VStack(alignment: .leading, spacing: 12) {
+           
+                            Text("\(testing.attributes?.name ?? "")")
+                                .font(.system(size: 20, weight: .bold))
+                                
+                            Text("\(testing.attributes?.fulladdr ?? "")")
+                                .lineLimit(2)
+                                .font(.subheadline)
+                            
+            
+                            
+                            Text("\(testing.attributes?.operationalhours ?? "")")
+                                .font(.caption)
+                                .fontWeight(.bold)
+                                .foregroundColor(.secondary)
+                            
+                  
+
+                        }
+                    }
+                    .padding(.vertical, 8)
+                }
+            }
+            
+            
+//             .padding(.top, 8)
+//             .padding(.horizontal, 20)
+//            // Lets the bottom card with the Text Field to not take up the entire width of the screen
+//         //   .frame(maxWidth: 712)
+//            .frame(height: 175)
+//            // Adding the BlurView Modifer to give a glass appearance
+//            .background(BlurView(style: .systemThinMaterial))
+//            .cornerRadius(30)
+//            .frame(maxWidth: .infinity)
+//           // .frame(maxWidth: .infinity)
+//            .listRowBackground(BlurView(style: .systemThinMaterial))
+        }
+        .navigationBarHidden(false)
     }
+      
+     
+    
+}
 }
 
 struct AllTestingList_Previews: PreviewProvider {
