521cfc9dd84eff5b959856aedef231d434cf70d2
Matthew Garlington <SBMatthewG@gmail.com>
2021-02-24 21:19:49 -0500

    feat: Added View to display Mass Vaccination Locations in a List View
    This view was added to display a list view of Mass Vaccination locations with a navigation view to furhter details about the selected list item

--- CovidVaccMD/View/VaccineList/MassVaccineList.swift
+++ CovidVaccMD/View/VaccineList/MassVaccineList.swift
@@ -8,11 +8,71 @@
 import SwiftUI
 
 struct MassVaccineList: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
+    @ObservedObject var massVaccinationsitesViewModel = MassVaccinationViewModel()
 
+    var body: some View {
+        NavigationView {
+        List {
+       
+            ForEach(massVaccinationsitesViewModel.vaccineLocationsMD?.features ?? [], id: \.self) { vaccine in
+                NavigationLink(destination: VaccineLocationsDetails(vaccine: vaccine)) {
+                    
+                    HStack {
+                        
+//                        Image(vaccine)
+//                            .resizable()
+//                            .aspectRatio(contentMode: .fit)
+//                            .frame(width: 80, height: 80)
+//                            .background(Color.black)
+//                            .cornerRadius(20)
+//                            .padding(.trailing, 4)
+                        
+            
+                        VStack(alignment: .leading, spacing: 12) {
+           
+                            Text("\(vaccine.attributes?.name ?? "")")
+                                .font(.system(size: 20, weight: .bold))
+                                
+                            Text("\(vaccine.attributes?.fulladdr ?? "")")
+                                .lineLimit(2)
+                                .font(.subheadline)
+                            
+            
+                            
+                            Text("\(vaccine.attributes?.operationalhours ?? "")")
+                                .font(.caption)
+                                .fontWeight(.bold)
+                                .foregroundColor(.secondary)
+                            
+
+                                
+                            
+                        }
+                    }
+                    .padding(.vertical, 8)
+                }
+            }
+            
+            
+//             .padding(.top, 8)
+//             .padding(.horizontal, 20)
+//            // Lets the bottom card with the Text Field to not take up the entire width of the screen
+//         //   .frame(maxWidth: 712)
+//            .frame(height: 175)
+//            // Adding the BlurView Modifer to give a glass appearance
+//            .background(BlurView(style: .systemThinMaterial))
+//            .cornerRadius(30)
+//            .frame(maxWidth: .infinity)
+//           // .frame(maxWidth: .infinity)
+//            .listRowBackground(BlurView(style: .systemThinMaterial))
+        }
+        .navigationBarHidden(false)
+    }
+      
+     
+    
+}
+}
 struct MassVaccineList_Previews: PreviewProvider {
     static var previews: some View {
         MassVaccineList()
