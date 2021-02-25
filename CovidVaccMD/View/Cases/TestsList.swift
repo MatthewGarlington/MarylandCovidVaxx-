ecf295e33b6e1b717e46250f92f76858951f15e1
Matthew Garlington <SBMatthewG@gmail.com>
2021-02-25 08:39:42 -0500

    feat: This view was added to show a list view for All test and 24 hour change in Tests for Covid 19 in Maryland

--- CovidVaccMD/View/Cases/TestsList.swift
+++ CovidVaccMD/View/Cases/TestsList.swift
@@ -8,10 +8,70 @@
 import SwiftUI
 
 struct TestsList: View {
+    @ObservedObject var casesModel = CovidMasterModel()
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+ 
+        NavigationView {
+        List {
+            ForEach(casesModel.allMDHeader.reversed(), id: \.self) { cases in
+       
+                    
+                    HStack {
+
+                        
+            
+                        VStack(alignment: .leading, spacing: 12) {
+                           
+                            
+                            HStack(spacing: 10) {
+                                 
+                           
+                               
+                                
+                                Text("\((cases.reportdate?.getDateFormatterForString(dateString: cases.reportdate ?? ""))!):")
+                                    .font(.system(size: 20, weight: .bold))
+                                
+                                
+                                Spacer()
+                                
+                                HStack(spacing: 10) {
+                                    Text("\(cases.totaltests ?? "")")
+                                        .font(.system(size: 20, weight: .semibold))
+                                    
+                                
+                                
+                                VStack(alignment: .leading) {
+                                    Text("+\(cases.testsdelta ?? "")")
+                                        .font(.caption)
+                                        .fontWeight(.bold)
+                                        .foregroundColor(.secondary)
+                                    Spacer()
+                                }
+                                }
+                            }
+                          
+                            
+                            
+                        
+ 
+                            
+                        }
+                    }
+                    .padding(.vertical, 8)
+                
+            }
+            .navigationBarTitle("History of Total Case numbers")
+            .navigationBarTitleDisplayMode(.inline)
+        }
+       
+    
+      
+        }
+    
 }
+}
+
 
 struct TestsList_Previews: PreviewProvider {
     static var previews: some View {
