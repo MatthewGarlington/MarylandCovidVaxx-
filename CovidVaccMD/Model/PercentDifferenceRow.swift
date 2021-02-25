52ee308f6a8b7cf94a0fc727e3b62a9811eef127
Matthew Garlington <SBMatthewG@gmail.com>
2021-02-25 17:53:32 -0500

    init commit

---
+++ CovidVaccMD/Model/PercentDifferenceRow.swift
@@ -0,0 +1,20 @@
+//
+//  PercentDifferenceRow.swift
+//  CovidVaccMD
+//
+//  Created by Matthew Garlington on 2/25/21.
+//
+
+import SwiftUI
+
+struct PercentDifferenceRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PercentDifferenceRow_Previews: PreviewProvider {
+    static var previews: some View {
+        PercentDifferenceRow()
+    }
+}
---
+++ CovidVaccMD/View/ProgressRing/PercentChangeView.swift
@@ -0,0 +1,20 @@
+//
+//  PercentChangeView.swift
+//  CovidVaccMD
+//
+//  Created by Matthew Garlington on 2/25/21.
+//
+
+import SwiftUI
+
+struct PercentChangeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PercentChangeView_Previews: PreviewProvider {
+    static var previews: some View {
+        PercentChangeView()
+    }
+}
