dbf40a821a3bc1ce11945acd178cefc3dd58d858
Matthew Garlington <SBMatthewG@gmail.com>
2021-02-25 08:31:56 -0500

    init commit

---
+++ CovidVaccMD/View/Cases/TestsList.swift
@@ -0,0 +1,20 @@
+//
+//  TestsList.swift
+//  CovidVaccMD
+//
+//  Created by Matthew Garlington on 2/25/21.
+//
+
+import SwiftUI
+
+struct TestsList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TestsList_Previews: PreviewProvider {
+    static var previews: some View {
+        TestsList()
+    }
+}
