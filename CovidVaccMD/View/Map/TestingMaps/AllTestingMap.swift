8872373e4f788510c89336b9b691fdfe3bc37992
Matthew Garlington <SBMatthewG@gmail.com>
2021-02-25 10:59:49 -0500

    feat: The View created shows a map view view of all testing locations in the state of Maryland that are active

--- CovidVaccMD/View/Map/TestingMaps/AllTestingMap.swift
+++ CovidVaccMD/View/Map/TestingMaps/AllTestingMap.swift
@@ -6,10 +6,77 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct AllTestingMap: View {
+
+    @State var annotations = [MKPointAnnotation]()
+    @State var pinsAllTesting = [MKPointAnnotation]()
+    @State var showingPlaceDetails = false
+    @State var selectedPlace: MKPointAnnotation?
+    @State var showTestingList = false
+   
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        ZStack(alignment: .top) {
+            
+        
+            
+            
+            VStack {
+                MapTestingView(annotations: $annotations, pinsAllTesting: $pinsAllTesting, selectedPlace: $selectedPlace, showingPlaceDetails: $showingPlaceDetails)
+                    
+                    .alert(isPresented: $showingPlaceDetails) {
+                        
+                        
+                        Alert(title: Text(selectedPlace?.title ?? ""), message: Text(selectedPlace?.subtitle ?? ""))
+                }
+                
+                
+            }
+            
+            ZStack {
+                
+           
+                    BlurView(style: .systemThickMaterial)
+                        .frame(maxWidth: .infinity)
+                        .cornerRadius(15)
+                        .frame(height: 75)
+                        .shadow(color: Color.black.opacity(0.2), radius: 10, x: 0, y: 12)
+                        .shadow(color: Color.black.opacity(0.1), radius: 1, x: 0, y: 1)
+                        .padding()
+            
+                HStack {
+                Text("All Testing Locations")
+                    .bold()
+                    
+                    
+                    Button(action: {self.showTestingList.toggle()}) {
+                        Image(systemName: "list.bullet")
+                            .foregroundColor(Color.red)
+                            .font(.system(size: 16, weight: .medium))
+                            .frame(width: 36, height: 36)
+                            .background(Color("background3"))
+                            .clipShape(Circle())
+                            .shadow(color: Color.black.opacity(0.1), radius: 1, x: 0, y: 1)
+                            .shadow(color: Color.black.opacity(0.2), radius: 10, x: 0, y: 10)
+                            
+                        
+                    }.offset(x: 50)
+                    .sheet(isPresented: $showTestingList) {
+                        
+                        MassVaccineList()
+                  
+                    }
+                    
+                    
+                    
+                }
+                    
+               
+            }
+         
+        }
     }
 }
 
