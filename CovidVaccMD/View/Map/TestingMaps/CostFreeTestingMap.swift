3258b3accb8f0c59fa2872a5e5faaf1bd352b13c
Matthew Garlington <SBMatthewG@gmail.com>
2021-02-25 10:59:14 -0500

    feat: Added Map view to display pins with annotations for Covid 19 Testing Locations in the state of Maryland that are free testing sites

--- CovidVaccMD/View/Map/TestingMaps/CostFreeTestingMap.swift
+++ CovidVaccMD/View/Map/TestingMaps/CostFreeTestingMap.swift
@@ -6,10 +6,54 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct CostFreeTestingMap: View {
+    @State var annotations = [MKPointAnnotation]()
+    @State var pinsCostFreeTesting = [MKPointAnnotation]()
+    @State var showingPlaceDetails = false
+    @State var selectedPlace: MKPointAnnotation?
+   
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        ZStack(alignment: .top) {
+            
+        
+            
+            
+            VStack {
+                CostFreeTestingView(annotations: $annotations, pinsCostFreeTesting: $pinsCostFreeTesting, selectedPlace: $selectedPlace, showingPlaceDetails: $showingPlaceDetails)
+                    
+                    .alert(isPresented: $showingPlaceDetails) {
+                        
+                        
+                        Alert(title: Text(selectedPlace?.title ?? ""), message: Text(selectedPlace?.subtitle ?? ""))
+                }
+                
+                
+            }
+            
+            ZStack {
+                
+                BlurView(style: .systemThickMaterial)
+                    .frame(maxWidth: .infinity)
+                    .cornerRadius(15)
+                    .frame(height: 75)
+                    .shadow(color: Color.black.opacity(0.2), radius: 10, x: 0, y: 12)
+                    .shadow(color: Color.black.opacity(0.1), radius: 1, x: 0, y: 1)
+                    .padding()
+                    
+                    
+                
+             
+                
+                Text("Cost Free Testing Locations")
+                    .bold()
+                    
+               
+            }
+         
+        }
     }
 }
 
