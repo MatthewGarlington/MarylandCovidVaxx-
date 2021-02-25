fdac5636bfb9c41b278f7a39feec1b39deab0ec5
Matthew Garlington <SBMatthewG@gmail.com>
2021-02-25 10:51:34 -0500

    feat: The view created presents a map with pin annotations of the Testing locations offering pediatric services

--- CovidVaccMD/View/Map/TestingMaps/PedsTestingMap.swift
+++ CovidVaccMD/View/Map/TestingMaps/PedsTestingMap.swift
@@ -6,12 +6,61 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct PedsTestingMap: View {
+    @State var annotations = [MKPointAnnotation]()
+    @State var pinsPedsTesting = [MKPointAnnotation]()
+    @State var showingPlaceDetails = false
+    @State var selectedPlace: MKPointAnnotation?
+   
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        ZStack(alignment: .top) {
+            
+        
+            
+            
+            VStack {
+                PedsTestingMapView(annotations: $annotations, pinsPedsTesting: $pinsPedsTesting, selectedPlace: $selectedPlace, showingPlaceDetails: $showingPlaceDetails)
+                    
+                    .alert(isPresented: $showingPlaceDetails) {
+                        
+                        
+                        Alert(title: Text(selectedPlace?.title ?? ""), message: Text(selectedPlace?.subtitle ?? ""))
+                }
+                
+                
+            }
+            
+            ZStack {
+                
+                BlurView(style: .systemThickMaterial)
+                    .frame(maxWidth: .infinity)
+                    .cornerRadius(15)
+                    .frame(height: 75)
+                    .shadow(color: Color.black.opacity(0.2), radius: 10, x: 0, y: 12)
+                    .shadow(color: Color.black.opacity(0.1), radius: 1, x: 0, y: 1)
+                    .padding()
+                    
+                    
+                
+             
+                VStack {
+                    Text("Pediatrics")
+                    .bold()
+                    Text("Testing Locations")
+                        .bold()
+                    
+                }
+                    
+               
+            }
+         
+        }
     }
 }
+
 
 struct PedsTestingMap_Previews: PreviewProvider {
     static var previews: some View {
