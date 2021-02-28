a76d6ec9ff0744db2696459ee9188d73a3df3aef
Matthew Garlington <SBMatthewG@gmail.com>
2021-02-28 15:50:37 -0500

    feat: Added funcationality for all Map Views within MapView to center the map on the user's location if permission is granted to use the user's location
    This functionality was added in order to display the location of the user in order to see vaccine locations that are local to the user in the State of Maryland. This was done by requesting location info from CLLocationManager and injecting authorization, span, region, and location of the user in to the updateUIView in each Map View

--- CovidVaccMD/View/Map/VaccineMaps/MapView.swift
+++ CovidVaccMD/View/Map/VaccineMaps/MapView.swift
@@ -7,12 +7,14 @@
 
 import SwiftUI
 import MapKit
+import CoreLocation
+import Combine
+
 
 
 struct MapViewMaryland: UIViewRepresentable {
     
-    //var annotations: MKPointAnnotation?
-    
+  
     @ObservedObject var localHealthVaccineLocations = LocalHealthVaccineLocationsModel()
     @ObservedObject var massVaccinationsitesViewModel = MassVaccinationViewModel()
     @ObservedObject var retailVaccinationsViewModel = RetailVaccinationViewModel()
@@ -24,42 +26,38 @@ struct MapViewMaryland: UIViewRepresentable {
     @Binding var pinsRetailArray: [MKPointAnnotation]
     @Binding var selectedPlace: MKPointAnnotation?
     @Binding var showingPlaceDetails: Bool
-
+    let locationManager = CLLocationManager()
+   
   
 
     func makeUIView(context: Context) -> MKMapView {
         let mapView = MKMapView()
-        let region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 39.045753, longitude: -76.641273), latitudinalMeters: CLLocationDistance(exactly: 300000)!, longitudinalMeters: CLLocationDistance(exactly: 300000)!)
-        mapView.setRegion(mapView.regionThatFits(region), animated: true)
+        mapView.userTrackingMode = .follow
         mapView.delegate = context.coordinator
         
+        
+        return mapView
 
-     return mapView
     }
 
     func updateUIView(_ view: MKMapView, context: Context) {
-
         
-        for i in stride(from: 0, through: (((localHealthVaccineLocations.vaccineLocationsMD?.features?.count ?? 0)-1)), by: 1)
-        {
-            
-            
+        view.showsUserLocation = true
+        let status = CLLocationManager.authorizationStatus()
+        locationManager.requestAlwaysAuthorization()
+        locationManager.requestWhenInUseAuthorization()
         
-        let pinsLocalHealthArray = MKPointAnnotation()
-            
-           
-
-
-            pinsLocalHealthArray.title = localHealthVaccineLocations.vaccineLocationsMD?.features?[i].attributes?.name ?? ""
-            pinsLocalHealthArray.coordinate = CLLocationCoordinate2D(latitude:(localHealthVaccineLocations.vaccineLocationsMD?.features?[i].geometry?.coordinate.latitude ?? 0.0), longitude: (localHealthVaccineLocations.vaccineLocationsMD?.features?[i].geometry?.coordinate.longitude ?? 0.0))
-            pinsLocalHealthArray.subtitle = localHealthVaccineLocations.vaccineLocationsMD?.features?[i].attributes?.fulladdr ?? ""
-        
-     
-            view.addAnnotation(pinsLocalHealthArray)
-              
-            
+        if status == .authorizedAlways || status == .authorizedWhenInUse {
+            locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+            locationManager.startUpdatingLocation()
+            let location: CLLocationCoordinate2D = locationManager.location!.coordinate
+            let span = MKCoordinateSpan(latitudeDelta: 0.25, longitudeDelta: 0.25)
+            let region = MKCoordinateRegion(center: location, span: span)
+            view.setRegion(region, animated: true)
         }
         
+ 
+ 
         for i in stride(from: 0, through: (((massVaccinationsitesViewModel.vaccineLocationsMD?.features?.count ?? 0)-1)), by: 1)
         {
             
@@ -142,11 +140,8 @@ struct MapViewMaryland: UIViewRepresentable {
             
         }
         func mapViewDidChangeVisibleRegion(_ view: MKMapView) {
-        //    parent.centerCoordinate = view.centerCoordinate
-          //  parent.region = view.region
-             
-            
-          
+    
+
          
         }
         
@@ -154,17 +149,14 @@ struct MapViewMaryland: UIViewRepresentable {
         func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
             if !(annotation is MKUserLocation) {
                 let pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: String(annotation.hash))
+                
 
                 let rightButton = UIButton(type: .detailDisclosure)
                 pinView.rightCalloutAccessoryView = rightButton
                 pinView.animatesDrop = false
                 pinView.canShowCallout = true
              
-              
-             
-               
-                
-                
+
 
                 return pinView
             }
@@ -186,6 +178,8 @@ struct MapViewMaryland: UIViewRepresentable {
     
     
 }
+
+
 
 extension MKPointAnnotation {
     static var example: MKPointAnnotation {
@@ -228,14 +222,17 @@ struct HospitalMapViewMaryland: UIViewRepresentable {
     @Binding var pinsHospitalArray: [MKPointAnnotation]
     @Binding var selectedPlace: MKPointAnnotation?
     @Binding var showingPlaceDetails: Bool
+    let locationManager = CLLocationManager()
+   
 
   
 
     func makeUIView(context: Context) -> MKMapView {
         let mapView = MKMapView()
-        let region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 39.045753, longitude: -76.641273), latitudinalMeters: CLLocationDistance(exactly: 300000)!, longitudinalMeters: CLLocationDistance(exactly: 300000)!)
-        mapView.setRegion(mapView.regionThatFits(region), animated: true)
+    
+        mapView.userTrackingMode = .follow
         mapView.delegate = context.coordinator
+        
         
 
      return mapView
@@ -243,7 +240,21 @@ struct HospitalMapViewMaryland: UIViewRepresentable {
 
     func updateUIView(_ view: MKMapView, context: Context) {
 
-   
+        view.showsUserLocation = true
+        let status = CLLocationManager.authorizationStatus()
+        locationManager.requestAlwaysAuthorization()
+        locationManager.requestWhenInUseAuthorization()
+        
+        if status == .authorizedAlways || status == .authorizedWhenInUse {
+            locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+            locationManager.startUpdatingLocation()
+            let location: CLLocationCoordinate2D = locationManager.location!.coordinate
+            let span = MKCoordinateSpan(latitudeDelta: 0.3, longitudeDelta: 0.3)
+            let region = MKCoordinateRegion(center: location, span: span)
+            view.setRegion(region, animated: true)
+        }
+      
+              
         
         for i in stride(from: 0, through: (((hospitalVaccinationsViewModel.vaccineLocationsMD?.features?.count ?? 0)-1)), by: 1)
         {
@@ -274,6 +285,7 @@ struct HospitalMapViewMaryland: UIViewRepresentable {
         Coordinator(self)
     }
 
+  
     class Coordinator: NSObject, MKMapViewDelegate {
         var parent: HospitalMapViewMaryland
 
@@ -283,8 +295,8 @@ struct HospitalMapViewMaryland: UIViewRepresentable {
             
         }
         func mapViewDidChangeVisibleRegion(_ view: MKMapView) {
-        //    parent.centerCoordinate = view.centerCoordinate
-          //  parent.region = view.region
+     
+      
              
             
           
@@ -295,17 +307,14 @@ struct HospitalMapViewMaryland: UIViewRepresentable {
         func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
             if !(annotation is MKUserLocation) {
                 let pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: String(annotation.hash))
+                
 
                 let rightButton = UIButton(type: .detailDisclosure)
                 pinView.rightCalloutAccessoryView = rightButton
                 pinView.animatesDrop = false
                 pinView.canShowCallout = true
              
-              
-             
-               
-                
-                
+
 
                 return pinView
             }
@@ -324,7 +333,6 @@ struct HospitalMapViewMaryland: UIViewRepresentable {
         
         
     }
-    
     
 }
 
@@ -345,13 +353,14 @@ struct RetailMapViewMaryland: UIViewRepresentable {
     @Binding var pinsRetailArray: [MKPointAnnotation]
     @Binding var selectedPlace: MKPointAnnotation?
     @Binding var showingPlaceDetails: Bool
+    let locationManager = CLLocationManager()
 
   
 
     func makeUIView(context: Context) -> MKMapView {
         let mapView = MKMapView()
-        let region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 39.045753, longitude: -76.641273), latitudinalMeters: CLLocationDistance(exactly: 300000)!, longitudinalMeters: CLLocationDistance(exactly: 300000)!)
-        mapView.setRegion(mapView.regionThatFits(region), animated: true)
+
+        mapView.userTrackingMode = .follow
         mapView.delegate = context.coordinator
         
 
@@ -360,7 +369,19 @@ struct RetailMapViewMaryland: UIViewRepresentable {
 
     func updateUIView(_ view: MKMapView, context: Context) {
 
-   
+        view.showsUserLocation = true
+        let status = CLLocationManager.authorizationStatus()
+        locationManager.requestAlwaysAuthorization()
+        locationManager.requestWhenInUseAuthorization()
+        
+        if status == .authorizedAlways || status == .authorizedWhenInUse {
+            locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+            locationManager.startUpdatingLocation()
+            let location: CLLocationCoordinate2D = locationManager.location!.coordinate
+            let span = MKCoordinateSpan(latitudeDelta: 0.3, longitudeDelta: 0.3)
+            let region = MKCoordinateRegion(center: location, span: span)
+            view.setRegion(region, animated: true)
+        }
         
         for i in stride(from: 0, through: (((retailVaccinationsViewModel.vaccineLocationsMD?.features?.count ?? 0)-1)), by: 1)
         {
@@ -382,10 +403,6 @@ struct RetailMapViewMaryland: UIViewRepresentable {
             
         }
         
-             
-     
-                
-
     }
     
     
@@ -404,10 +421,7 @@ struct RetailMapViewMaryland: UIViewRepresentable {
             
         }
         func mapViewDidChangeVisibleRegion(_ view: MKMapView) {
-        //    parent.centerCoordinate = view.centerCoordinate
-          //  parent.region = view.region
-             
-            
+  
           
          
         }
@@ -466,13 +480,13 @@ struct MassMapViewMaryland: UIViewRepresentable {
     @Binding var pinsMassArray: [MKPointAnnotation]
     @Binding var selectedPlace: MKPointAnnotation?
     @Binding var showingPlaceDetails: Bool
+    let locationManager = CLLocationManager()
 
   
 
     func makeUIView(context: Context) -> MKMapView {
         let mapView = MKMapView()
-        let region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 39.045753, longitude: -76.641273), latitudinalMeters: CLLocationDistance(exactly: 300000)!, longitudinalMeters: CLLocationDistance(exactly: 300000)!)
-        mapView.setRegion(mapView.regionThatFits(region), animated: true)
+        mapView.userTrackingMode = .follow
         mapView.delegate = context.coordinator
         
 
@@ -481,7 +495,19 @@ struct MassMapViewMaryland: UIViewRepresentable {
 
     func updateUIView(_ view: MKMapView, context: Context) {
 
-   
+        view.showsUserLocation = true
+        let status = CLLocationManager.authorizationStatus()
+        locationManager.requestAlwaysAuthorization()
+        locationManager.requestWhenInUseAuthorization()
+        
+        if status == .authorizedAlways || status == .authorizedWhenInUse {
+            locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+            locationManager.startUpdatingLocation()
+            let location: CLLocationCoordinate2D = locationManager.location!.coordinate
+            let span = MKCoordinateSpan(latitudeDelta: 0.3, longitudeDelta: 0.3)
+            let region = MKCoordinateRegion(center: location, span: span)
+            view.setRegion(region, animated: true)
+        }
         
         for i in stride(from: 0, through: (((massVaccinationsitesViewModel.vaccineLocationsMD?.features?.count ?? 0)-1)), by: 1)
         {
@@ -525,11 +551,7 @@ struct MassMapViewMaryland: UIViewRepresentable {
             
         }
         func mapViewDidChangeVisibleRegion(_ view: MKMapView) {
-        //    parent.centerCoordinate = view.centerCoordinate
-          //  parent.region = view.region
-             
-            
-          
+
          
         }
         
@@ -586,13 +608,13 @@ struct LocalHealthMapViewMaryland: UIViewRepresentable {
     @Binding var pinsLocalHealthArray: [MKPointAnnotation]
     @Binding var selectedPlace: MKPointAnnotation?
     @Binding var showingPlaceDetails: Bool
+    let locationManager = CLLocationManager()
 
   
 
     func makeUIView(context: Context) -> MKMapView {
         let mapView = MKMapView()
-        let region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 39.045753, longitude: -76.641273), latitudinalMeters: CLLocationDistance(exactly: 300000)!, longitudinalMeters: CLLocationDistance(exactly: 300000)!)
-        mapView.setRegion(mapView.regionThatFits(region), animated: true)
+        mapView.userTrackingMode = .follow
         mapView.delegate = context.coordinator
         
 
@@ -601,7 +623,19 @@ struct LocalHealthMapViewMaryland: UIViewRepresentable {
 
     func updateUIView(_ view: MKMapView, context: Context) {
 
-   
+        view.showsUserLocation = true
+        let status = CLLocationManager.authorizationStatus()
+        locationManager.requestAlwaysAuthorization()
+        locationManager.requestWhenInUseAuthorization()
+        
+        if status == .authorizedAlways || status == .authorizedWhenInUse {
+            locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+            locationManager.startUpdatingLocation()
+            let location: CLLocationCoordinate2D = locationManager.location!.coordinate
+            let span = MKCoordinateSpan(latitudeDelta: 0.3, longitudeDelta: 0.3)
+            let region = MKCoordinateRegion(center: location, span: span)
+            view.setRegion(region, animated: true)
+        }
         
         for i in stride(from: 0, through: (((localHealthVaccineLocations.vaccineLocationsMD?.features?.count ?? 0)-1)), by: 1)
         {
@@ -645,11 +679,7 @@ struct LocalHealthMapViewMaryland: UIViewRepresentable {
             
         }
         func mapViewDidChangeVisibleRegion(_ view: MKMapView) {
-        //    parent.centerCoordinate = view.centerCoordinate
-          //  parent.region = view.region
-             
-            
-          
+
          
         }
         
@@ -697,4 +727,5 @@ struct LocalHealthMapViewMaryland_Previews: PreviewProvider {
         LocalHealthMapViewMaryland(annotations: .constant(MKPointAnnotation.exampleArray), pinsLocalHealthArray: .constant(MKPointAnnotation.exampleArray), selectedPlace: .constant(MKPointAnnotation.example), showingPlaceDetails: .constant(true))
     }
 }
+
 
