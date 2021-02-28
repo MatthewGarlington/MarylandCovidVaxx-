0c4ec97107f2e4fdfa9e1611c92165114144931e
Matthew Garlington <SBMatthewG@gmail.com>
2021-02-28 15:46:47 -0500

    feat: Added Toggle button to left of title in Title Card Zstack
    This was added in order to set a up a toggle button that will be given added functionality to toggle between a map view center on the user's location or if the user does not wish to share their location, display a map view that shows all of the State of Maryland as an overview for all vaccine locations in the State of Maryland

--- CovidVaccMD/View/Map/VaccineMaps/MapViewWithAnnotations.swift
+++ CovidVaccMD/View/Map/VaccineMaps/MapViewWithAnnotations.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 import MapKit
+import CoreLocation
 
 struct MapViewWithAnnotations: View {
     
@@ -18,49 +19,81 @@ struct MapViewWithAnnotations: View {
     @State var pinsHospitalArray = [MKPointAnnotation]()
     @State var showingPlaceDetails = false
     @State var selectedPlace: MKPointAnnotation?
-   
+    
+ 
+    @State private var showGreeting = true
+
     var body: some View {
         
-        ZStack(alignment: .top) {
-            
         
-            
-            
-            VStack {
-                MapViewMaryland(annotations: $annotations, pinsLocalHealthArray: $pinsLocalHealthArray, pinsHospitalArray: $pinsHospitalArray, pinsMassArray: $pinsMassArray, pinsRetailArray: $pinsRetailArray, selectedPlace: $selectedPlace, showingPlaceDetails: $showingPlaceDetails)
-                    
-                    .alert(isPresented: $showingPlaceDetails) {
-                        
-                        
-                        Alert(title: Text(selectedPlace?.title ?? ""), message: Text(selectedPlace?.subtitle ?? ""))
-                }
-                
-                
-            }
-            
-            ZStack {
-                
-                BlurView(style: .systemThickMaterial)
-                    .frame(maxWidth: .infinity)
-                    .cornerRadius(15)
-                    .frame(height: 75)
-                    .shadow(color: Color.black.opacity(0.2), radius: 10, x: 0, y: 12)
-                    .shadow(color: Color.black.opacity(0.1), radius: 1, x: 0, y: 1)
-                    .padding()
-                    
-                    
+
+            ZStack(alignment: .top) {
                 
              
+            
                 
-                Text("All Vaccine Locations")
-                    .bold()
+                
+                VStack {
                     
+//                    if showGreeting {
+                    MapViewMaryland(annotations: $annotations, pinsLocalHealthArray: $pinsLocalHealthArray, pinsHospitalArray: $pinsHospitalArray, pinsMassArray: $pinsMassArray, pinsRetailArray: $pinsRetailArray, selectedPlace: $selectedPlace, showingPlaceDetails: $showingPlaceDetails)
+
+                        .alert(isPresented: $showingPlaceDetails) {
+
+
+                            Alert(title: Text(selectedPlace?.title ?? ""), message: Text(selectedPlace?.subtitle ?? ""))
+                    }
+                    
+//                    } else {
+//
+//                    // Map View with locations
+//                    }
+                    
+                }
+                
+                VStack {
+                    ZStack(alignment: .leading) {
+                        
+                        BlurView(style: .systemThickMaterial)
+                            .frame(maxWidth: .infinity)
+                            .cornerRadius(15)
+                            .frame(height: 75)
+                            .shadow(color: Color.black.opacity(0.2), radius: 10, x: 0, y: 12)
+                            .shadow(color: Color.black.opacity(0.1), radius: 1, x: 0, y: 1)
+                            .padding()
+                            
+                            
+                        
+                    
+                        HStack(spacing: 0) {
+                            VStack {
+                                Toggle("", isOn: $showGreeting)
+                                    .toggleStyle(SwitchToggleStyle(tint: .red))
+                            }
+                            .frame(width: 125, height: 150)
+                            .offset(x: -30)
+
+//                                           if showGreeting {
+//                                               Text("")
+//                                           }
+                            Text("All Vaccine Locations")
+                                .bold()
+                        }
+                            
                
+                    }
+                    
+                    
+                }
+                
+        
+             
             }
-         
+       
         }
+        
     }
-}
+
 
 struct MapViewWithAnnotations_Previews: PreviewProvider {
     static var previews: some View {
