e0b5d300cab64bf0f9c9653b8091a4de4760a62e
Matthew Garlington <SBMatthewG@gmail.com>
2021-02-28 15:14:25 -0500

    testing: Testing showing current location in Map view

---
+++ CovidVaccMD/View/Map/VaccineMaps/MapviewPractice.swift
@@ -0,0 +1,20 @@
+//
+//  MapviewPractice.swift
+//  CovidVaccMD
+//
+//  Created by Matthew Garlington on 2/28/21.
+//
+
+import SwiftUI
+
+struct MapviewPractice: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MapviewPractice_Previews: PreviewProvider {
+    static var previews: some View {
+        MapviewPractice()
+    }
+}
