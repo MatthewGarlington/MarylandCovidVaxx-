78434b385f6a2bfe4bed331d2a716df52d4c62cc
Matthew Garlington <SBMatthewG@gmail.com>
2021-03-01 14:00:25 -0500

    feat: This view was added in order to show a list of Second doses given each day since data was collected
    This feature was added to give more historical context on the amount of second doses given. The date was formatted using a custom Int extension that takes in the integer and formats it to a short date format

--- CovidVaccMD/View/Vaccine/SecondDoseList.swift
+++ CovidVaccMD/View/Vaccine/SecondDoseList.swift
@@ -8,9 +8,59 @@
 import SwiftUI
 
 struct SecondDoseList: View {
+    @ObservedObject var vaccineHistoricalViewModel = HistoricalVaccineData()
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+ 
+        NavigationView {
+        List {
+            ForEach(vaccineHistoricalViewModel.recentMDVaccineTotals?.features?.reversed() ?? [], id: \.self) { vaccine in
+       
+                    
+                    HStack {
+
+                        
+            
+                        VStack(alignment: .leading, spacing: 12) {
+                           
+                            
+                            HStack(spacing: 40) {
+                                 
+                                
+                               
+                                
+                                Text("\((vaccine.attributes?.CumulativeTotalVaccinatedDate?.getDateFromCrazyInt(date: vaccine.attributes!.CumulativeTotalVaccinatedDate!))!):")
+                                        .font(.system(size: 20, weight: .bold))
+                       
+                     
+                                VStack(alignment: .leading) {
+                                    Text("\(vaccine.attributes?.CumulativeTotalVaccinatedDoseTw ?? 0)")
+                                            .font(.system(size: 20, weight: .semibold))
+                                       
+                                        
+                                }
+
+                            }
+                          
+                            
+                            
+                        
+ 
+                            
+                        }
+                    }
+                    .padding(.vertical, 8)
+                
+            }
+          
+        }
+       
+        .navigationBarTitle("History of Second Doses Given")
+        .navigationBarTitleDisplayMode(.inline)
+      
+        }
+    
+}
 }
 
 struct SecondDoseList_Previews: PreviewProvider {
