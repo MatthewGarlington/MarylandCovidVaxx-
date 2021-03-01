253b7b3137b79dbb6d07e2775f01fa696d7bf0bc
Matthew Garlington <SBMatthewG@gmail.com>
2021-03-01 10:50:01 -0500

    feat: Added toggle button to the leading position in the HStack of the title card that toggles between a map view centered on the state of maryland and a map view that is centered on the User's location
    This toggle button was added in order for the user to see the entire state of Maryland with all vaccine locations, or if the user gives permission, a map view will persent based on their location to give context of all vaccine locations nearest them on the map

--- CovidVaccMD/View/Map/VaccineMaps/MapViewWithAnnotations.swift
+++ CovidVaccMD/View/Map/VaccineMaps/MapViewWithAnnotations.swift
@@ -11,7 +11,7 @@ import CoreLocation
 
 struct MapViewWithAnnotations: View {
     
-
+    
     @State var annotations = [MKPointAnnotation]()
     @State var pinsLocalHealthArray = [MKPointAnnotation]()
     @State var pinsMassArray = [MKPointAnnotation]()
@@ -19,80 +19,98 @@ struct MapViewWithAnnotations: View {
     @State var pinsHospitalArray = [MKPointAnnotation]()
     @State var showingPlaceDetails = false
     @State var selectedPlace: MKPointAnnotation?
+    @State private var showUserLocation = false
     
- 
-    @State private var showGreeting = true
-
     var body: some View {
         
         
-
-            ZStack(alignment: .top) {
-                
-             
-            
-                
-                
-                VStack {
-                    
-//                    if showGreeting {
-                    MapViewMaryland(annotations: $annotations, pinsLocalHealthArray: $pinsLocalHealthArray, pinsHospitalArray: $pinsHospitalArray, pinsMassArray: $pinsMassArray, pinsRetailArray: $pinsRetailArray, selectedPlace: $selectedPlace, showingPlaceDetails: $showingPlaceDetails)
-
-                        .alert(isPresented: $showingPlaceDetails) {
-
-
-                            Alert(title: Text(selectedPlace?.title ?? ""), message: Text(selectedPlace?.subtitle ?? ""))
-                    }
-                    
-//                    } else {
-//
-//                    // Map View with locations
-//                    }
-                    
-                }
-                
-                VStack {
-                    ZStack(alignment: .leading) {
-                        
-                        BlurView(style: .systemThickMaterial)
-                            .frame(maxWidth: .infinity)
-                            .cornerRadius(15)
-                            .frame(height: 75)
-                            .shadow(color: Color.black.opacity(0.2), radius: 10, x: 0, y: 12)
-                            .shadow(color: Color.black.opacity(0.1), radius: 1, x: 0, y: 1)
-                            .padding()
-                            
-                            
-                        
-                    
-                        HStack(spacing: 0) {
-                            VStack {
-                                Toggle("", isOn: $showGreeting)
-                                    .toggleStyle(SwitchToggleStyle(tint: .red))
-                            }
-                            .frame(width: 125, height: 150)
-                            .offset(x: -30)
-
-//                                           if showGreeting {
-//                                               Text("")
-//                                           }
-                            Text("All Vaccine Locations")
-                                .bold()
-                        }
-                            
-               
-                    }
-                    
-                    
-                }
-                
         
-             
+        ZStack(alignment: .top) {
+            
+            
+            
+            
+            
+            VStack {
+                
+                if showUserLocation {
+                    
+                    //                    // Map View with User Locations
+                    MapViewMarylandUserLocationEnabled(annotations: $annotations, pinsLocalHealthArray: $pinsLocalHealthArray, pinsHospitalArray: $pinsHospitalArray, pinsMassArray: $pinsMassArray, pinsRetailArray: $pinsRetailArray, selectedPlace: $selectedPlace, showingPlaceDetails: $showingPlaceDetails)
+                        
+                        .alert(isPresented: $showingPlaceDetails) {
+                            
+                            
+                            Alert(title: Text(selectedPlace?.title ?? ""), message: Text(selectedPlace?.subtitle ?? ""))
+                        }
+                    
+                    
+                    
+                } else {
+                    
+                    // Map View with User Locations
+                    
+                    MapViewMaryland(annotations: $annotations, pinsLocalHealthArray: $pinsLocalHealthArray, pinsHospitalArray: $pinsHospitalArray, pinsMassArray: $pinsMassArray, pinsRetailArray: $pinsRetailArray, selectedPlace: $selectedPlace, showingPlaceDetails: $showingPlaceDetails)
+                        
+                        
+                        .alert(isPresented: $showingPlaceDetails) {
+                            
+                            
+                            Alert(title: Text(selectedPlace?.title ?? ""), message: Text(selectedPlace?.subtitle ?? ""))
+                        }
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                }
+                
             }
-       
+            
+            VStack {
+                ZStack(alignment: .leading) {
+                    
+                    BlurView(style: .systemThickMaterial)
+                        .frame(maxWidth: .infinity)
+                        .cornerRadius(15)
+                        .frame(height: 75)
+                        .shadow(color: Color.black.opacity(0.2), radius: 10, x: 0, y: 12)
+                        .shadow(color: Color.black.opacity(0.1), radius: 1, x: 0, y: 1)
+                        .padding()
+                    
+                    
+                    
+                    
+                    HStack(spacing: 0) {
+                        VStack {
+                            
+                            Toggle("", isOn: $showUserLocation.animation(.interactiveSpring(response: 2, dampingFraction: 0.5, blendDuration: 0.5)))
+                                .toggleStyle(SwitchToggleStyle(tint: .red))
+                            
+                        }
+                        .frame(width: 125)
+                        .offset(x: -30)
+                        
+                        
+                        Text("All Vaccine Locations")
+                            .bold()
+                    }
+                    
+                    
+                }
+                
+                
+            }
+            
+            
+            
         }
         
     }
+    
+}
 
 
 struct MapViewWithAnnotations_Previews: PreviewProvider {
