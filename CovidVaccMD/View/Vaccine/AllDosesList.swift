59c2865e2ca0e460bec87abd8db75efe3df263e8
Matthew Garlington <SBMatthewG@gmail.com>
2021-03-01 14:20:45 -0500

    feat: Added List View for All Vaccine Doses given in Maryland for Covid 19
    This List view was added to give historical data for All vaccine doses given in the State of Maryland for Covid 19 broken down by each day since the data was entered. A custom Date formatter uses the Int from the API call for date and turns it into a formatted date

--- CovidVaccMD/View/Vaccine/AllDosesList.swift
+++ CovidVaccMD/View/Vaccine/AllDosesList.swift
@@ -8,9 +8,59 @@
 import SwiftUI
 
 struct AllDosesList: View {
+    @ObservedObject var vaccineHistoricalViewModel = HistoricalVaccineData()
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+ 
+        NavigationView {
+        List {
+            ForEach(vaccineHistoricalViewModel.recentMDVaccineTotals?.features?.reversed() ?? [], id: \.self) { vaccine in
+       
+                    
+                    HStack {
+
+                        
+            
+                        VStack(alignment: .leading, spacing: 12) {
+                           
+                            
+                            HStack(spacing: 40) {
+                                 
+                                
+                               
+                                
+                                Text("\((vaccine.attributes?.CumulativeTotalVaccinatedDate?.getDateFromCrazyInt(date: vaccine.attributes!.CumulativeTotalVaccinatedDate!))!):")
+                                        .font(.system(size: 20, weight: .bold))
+                       
+                     
+                                VStack(alignment: .leading) {
+                                    Text("\(vaccine.attributes?.CumulativeTotalVaccinated ?? 0)")
+                                            .font(.system(size: 20, weight: .semibold))
+                                       
+                                        
+                                }
+
+                            }
+                          
+                            
+                            
+                        
+ 
+                            
+                        }
+                    }
+                    .padding(.vertical, 8)
+                
+            }
+          
+        }
+       
+        .navigationBarTitle("History of Total Vaccinations")
+        .navigationBarTitleDisplayMode(.inline)
+      
+        }
+    
+}
 }
 
 struct AllDosesList_Previews: PreviewProvider {
