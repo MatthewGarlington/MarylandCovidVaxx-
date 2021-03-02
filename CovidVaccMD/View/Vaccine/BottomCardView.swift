a89b7a23eeab9b4da1f9ec63cf3b432737d815dc
Matthew Garlington <SBMatthewG@gmail.com>
2021-03-02 12:34:01 -0500

    feat: Added Ringviews/ Progress rings for Testing and Vaccines
    This was added to present all progress rings shown through out the app in one view by stacking them on each other in a VStack and a title card next to each for context in an HStack

--- CovidVaccMD/View/Vaccine/BottomCardView.swift
+++ CovidVaccMD/View/Vaccine/BottomCardView.swift
@@ -8,7 +8,9 @@
 import SwiftUI
 
 struct BottomCardView: View {
-    @Binding var show: Bool
+    @ObservedObject var totalVaccineViewModel = TotalVaccineData()
+    @ObservedObject var positiveCasesViewModel = PositiveCasesViewModel()
+
     
     var body: some View {
         VStack(spacing: 20) {
@@ -16,22 +18,22 @@ struct BottomCardView: View {
                 .frame(width: 40, height: 5)
                 .cornerRadius(3)
                 .opacity(0.1)
+                .padding()
             
-            Text("This certifcate is proof Matthew Garlington has achieved the UI Design course with approval from a Design+Code instructor")
-                .multilineTextAlignment(.center)
-                .lineSpacing(4)
-                .font(.subheadline)
+            Text("Covd 19 Vaccine Percentages")
+                .bold()
+            
+            VStack(spacing: 30) {
+               
             HStack(spacing: 20) {
-                RingView(color1: #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1), color2: #colorLiteral(red: 0.2196078449, green: 0.007843137719, blue: 0.8549019694, alpha: 1), width: 88, height: 88, percent: 78, show: $show)
+                RingView(color1: #colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1), color2: #colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1), width: 100, height: 100, percent:  Double(totalVaccineViewModel.recentMDVaccineTotals?.features?[2].attributes?.Value ?? Double(0)), show: .constant(true))
                     .animation(Animation.easeInOut)
                 
                 VStack(alignment: .leading, spacing: 8) {
-                    Text("SwiftUI").fontWeight(.bold)
-                    
-                    Text("12 of 12 sections completed\n10 hours spent so far")
-                        .font(.footnote)
-                        .foregroundColor(.gray)
-                        .lineSpacing(4)
+                    Text("Percent of Population").modifier(FontModifer(style: .subheadline))
+                    Text("Testing Positive")
+                        .bold()
+                        .modifier(FontModifer(style: .caption))
                 }
                 .padding()
                 .background(Color("background3"))
@@ -39,22 +41,74 @@ struct BottomCardView: View {
                 .shadow(color: Color.black.opacity(0.2), radius: 20, x: 0, y: 10)
                 
             }
+                HStack(spacing: 20) {
+                    RingView(color1: #colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1), color2: #colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1), width: 100, height: 100, percent:  Double(totalVaccineViewModel.recentMDVaccineTotals?.features?[5].attributes?.Value ?? Double(0)), show: .constant(true))
+                        .animation(Animation.easeInOut)
+                    
+                    VStack(alignment: .leading, spacing: 8) {
+                        Text("Percent of Population").modifier(FontModifer(style: .subheadline))
+                        Text("2nd Doses Given")
+                            .bold()
+                            .modifier(FontModifer(style: .caption))
+                    }
+                    .padding()
+                    .background(Color("background3"))
+                    .cornerRadius(20)
+                    .shadow(color: Color.black.opacity(0.2), radius: 20, x: 0, y: 10)
+                    
+                }
+            }
+            
+            
+            VStack(spacing: 20) {
                 
-            Spacer()
+                Text("Covd 19 Testing Percentages")
+                    .bold()
+            
+            
+            VStack {
+               
+            HStack(spacing: 20) {
+                RingView2(color1: #colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1), color2: #colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1), width: 100, height: 100, percent:  Double(positiveCasesViewModel.positiveCases?.features?[(positiveCasesViewModel.positiveCases?.features!.count)! - 1].attributes?.rolling_avg ?? Double(0)), show: .constant(true))
+                    .animation(Animation.easeInOut)
+                
+                VStack(alignment: .leading, spacing: 8) {
+                    Text("Percent of Population").modifier(FontModifer(style: .subheadline))
+                    Text("Testing Positive")
+                        .bold()
+                        .modifier(FontModifer(style: .caption))
+                }
+                .padding()
+                .background(Color("background3"))
+                .cornerRadius(20)
+                .shadow(color: Color.black.opacity(0.2), radius: 20, x: 0, y: 10)
+                
+            }
+       
+            }
+                
             
                 
         }
-        .padding(.top, 8)
-        .padding(.horizontal, 20)
-        // Lets the bottom card with the Text Field to not take up the entire width of the screen
-        .frame(maxWidth: 712)
-        .frame(height: 800)
-        // Adding the BlurView Modifer to give a glass appearance
-        .background(BlurView(style: .systemThinMaterial))
-        .cornerRadius(30)
-        .shadow(radius: 20)
-        // This centers the change of the bottom card
-        .frame(maxWidth: .infinity)
+            
+            
+        Spacer()
+            
+        }
+        
+//        .padding(.top, 8)
+//        .padding(.horizontal, 20)
+//        // Lets the bottom card with the Text Field to not take up the entire width of the screen
+//        .frame(maxWidth: 712)
+//        .frame(height: 800)
+//        // Adding the BlurView Modifer to give a glass appearance
+//        .background(BlurView(style: .systemThinMaterial))
+//        .cornerRadius(30)
+//        .shadow(radius: 20)
+//        // This centers the change of the bottom card
+//        .frame(maxWidth: .infinity)
        
     }
 }
+
+
