048233ee6231feb2811e4ff5e76c7c5e177e5bf5
Matthew Garlington <SBMatthewG@gmail.com>
2021-03-02 12:30:51 -0500

    refactor: Cleaned up the Code in this view by taking away uneeded Observed objects from testing

--- CovidVaccMD/View/Vaccine/Home.swift
+++ CovidVaccMD/View/Vaccine/Home.swift
@@ -13,6 +13,10 @@ struct Home: View {
     @State var showContent = false
     @State var showList = false
     @EnvironmentObject var user: UserStore
+    @State var show = false
+    @State var showCard = false
+    @State var bottomState = CGSize.zero
+    @State var showFull = false
    
     
 
@@ -26,74 +30,13 @@ struct Home: View {
             
       
             HomeBackgroundView(showProfile: $showProfile)
-                .offset(y: showProfile ? -450 : 0)
-                .rotation3DEffect(Angle(degrees: showProfile ? Double(viewState.height / 10) - 10 : 0), axis: (x: 10.0, y: 0, z: 0))
-                .scaleEffect(showProfile ? 0.9 : 1)
-                .animation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0))
-                .edgesIgnoringSafeArea(.all)
+
             
             
             
 
             HomeView(showProfile: $showProfile, showContent: $showContent, viewState: $viewState)
 
-
-            
-          //   Direct the user to the login screen if a profile is not logged into
-            if user.showLogin {
-
-                ZStack {
-                
-
-                    VStack {
-                        HStack {
-                            Spacer()
-
-                            Image(systemName: "xmark")
-                                .frame(width: 36, height: 36)
-                                .foregroundColor(.white)
-                                .background(Color.black)
-                                .clipShape(Circle())
-                        }
-                        Spacer()
-                    }
-                    .padding()
-                    .onTapGesture {
-                        self.user.showLogin = false
-                    }
-                }
-            }
-            
-            if showContent {
-                
-                BlurView(style: .systemMaterial).ignoresSafeArea(edges: .bottom)
-                
-                ContentView()
-          
-              
-                VStack {
-                    HStack {
-                        Spacer()
-                      
-                        Image(systemName: "xmark")
-                            .frame(width: 36, height: 36)
-                            .foregroundColor(.white)
-                            .background(Color.black)
-                            .clipShape(Circle())
-                    }
-                    Spacer()
-                }
-                .offset(x: -16, y: 16)
-                .transition(.move(edge: .top))
-                .animation(.spring(response: 0.6, dampingFraction: 0.8, blendDuration: 0))
-                .onTapGesture {
-                    self.showContent = false
-                }
-                
-         
-            }
-            
-  
         }
     }
 }
