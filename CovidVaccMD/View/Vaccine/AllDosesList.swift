cf2bd5daed58548c50e899975adf213651bc6aa6
Matthew Garlington <SBMatthewG@gmail.com>
2021-03-02 20:25:52 -0500

    style: Added a list card
    This list card is interactive and gives the user an option to press to open to a larger view of the lsit or can be scrolled in the smaller card mode to present historical data with obstructing the view of the Homeview. A blur effect overlays the Homeview on activation to focus on the list card view

--- CovidVaccMD/View/Vaccine/AllDosesList.swift
+++ CovidVaccMD/View/Vaccine/AllDosesList.swift
@@ -9,51 +9,114 @@ import SwiftUI
 
 struct AllDosesList: View {
     @ObservedObject var vaccineHistoricalViewModel = HistoricalVaccineData()
-    
-
+    @State var viewState = CGSize.zero
+    @State var showCard = false
+    @State var bottomState = CGSize.zero
+    @State var showFull = false
+ 
 
     var body: some View {
  
-        NavigationView {
-        List {
-            ForEach(vaccineHistoricalViewModel.recentMDVaccineTotals?.features?.reversed() ?? [], id: \.self) { vaccine in
-       
-                    
-                    HStack {
-
-                        
-            
-                        VStack(alignment: .leading, spacing: 12) {
-                           
-                            
-                            HStack(spacing: 40) {
-                                 
-                                
-                               
-                                
-                                Text("\((vaccine.attributes?.CumulativeTotalVaccinatedDate?.getDateFromCrazyInt(date: vaccine.attributes!.CumulativeTotalVaccinatedDate!))!):")
-                                        .font(.system(size: 20, weight: .bold))
-                       
-                     
-                                VStack(alignment: .leading) {
-                                    Text("\(vaccine.attributes?.CumulativeTotalVaccinated ?? 0)")
-                                            .font(.system(size: 20, weight: .semibold))
-                                    
-                                 
-                                }
-
-                            }
-                        }
-                    }
-                    .padding(.vertical, 8)
                 
-            }
-          
-        }
+        VStack {
+     
+            ZStack {
+                
        
-        .navigationBarTitle("History of Total Vaccinations")
-        .navigationBarTitleDisplayMode(.inline)
-      
+                   
+                
+                BlurView(style: .systemUltraThinMaterial)
+                    
+                    .frame(width: 400, height: showCard ? 900 : 400, alignment: .center)
+                    .cornerRadius(40)
+                    .ignoresSafeArea(showCard ? .all : .keyboard)
+                   
+                
+                VStack {
+                    
+                    ZStack {
+                        
+                        BlurView(style: .systemUltraThinMaterialDark)
+                            
+                            .frame(width: 250, height: 50, alignment: .center)
+                            .cornerRadius(20)
+                    
+                    Text("History of Total Vaccines")
+                        .bold()
+                       
+                        
+                        
+                        
+                        
+                    }
+                    .offset(y: showCard ? 50 : 0)
+                    .padding()
+                
+                List {
+                
+            
+                        VStack {
+                        ForEach(vaccineHistoricalViewModel.recentMDVaccineTotals?.features?.reversed() ?? [], id: \.self) { vaccine in
+                   
+                                
+                                HStack {
+
+                                    
+                        
+                                    VStack(alignment: .leading, spacing: 12) {
+                                       
+                                        
+                                        HStack(spacing: 40) {
+                                             
+                                            
+                                           
+                                            
+                                            Text("\((vaccine.attributes?.CumulativeTotalVaccinatedDate?.getDateFromCrazyInt(date: vaccine.attributes!.CumulativeTotalVaccinatedDate!))!):")
+                                                    .font(.system(size: 20, weight: .bold))
+                                   
+                                 
+                                            VStack(alignment: .leading) {
+                                                Text("\(vaccine.attributes?.CumulativeTotalVaccinated ?? 0)")
+                                                        .font(.system(size: 20, weight: .semibold))
+                                                
+                                             
+                                            }
+
+                                        }
+                                    }
+                                }
+                                .padding(.vertical, 8)
+                        }
+                        
+                            
+                        }
+                 
+              
+                }
+                
+            
+                
+                
+                .frame(maxWidth: showCard ? 340 : 340)
+                .frame(maxHeight: showCard ? 800 : 220)
+             //   .background(Color.black)
+        //              .cornerRadius(20)
+                .clipShape(RoundedRectangle(cornerRadius: showCard ? 30 : 20, style: .continuous))
+                .shadow(radius: 20)
+                .offset(x: viewState.width, y: viewState.height)
+                .offset(y: showCard ? 80 : 0)
+                .blendMode(.hardLight)
+                .animation(.spring(response: 0.3, dampingFraction: 0.6, blendDuration: 0))
+                .onTapGesture {
+                    showCard.toggle()
+                  
+            
+            }
+
+                
+           
+            }
+            }
         }
     
 }
@@ -61,6 +124,8 @@ struct AllDosesList: View {
 
 struct AllDosesList_Previews: PreviewProvider {
     static var previews: some View {
+       
         AllDosesList()
+
     }
 }
