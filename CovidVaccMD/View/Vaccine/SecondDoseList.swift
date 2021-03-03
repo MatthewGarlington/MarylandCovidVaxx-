e8b1c76b3a67bea1e57b218f8ffb1e8d7026f860
Matthew Garlington <SBMatthewG@gmail.com>
2021-03-02 20:15:55 -0500

    style: Changed the list view to present in a small card that can be tapped to enlarge
    This styling was added in order to give the user a more animated view of the list with a small pop up that is centered in the middle of the screen. The background around the list card is blurred and the title aboce reflects the information beind shown in the list. The list can be pressed to enlarge the list view with a custom Spring animation

--- CovidVaccMD/View/Vaccine/SecondDoseList.swift
+++ CovidVaccMD/View/Vaccine/SecondDoseList.swift
@@ -9,55 +9,114 @@ import SwiftUI
 
 struct SecondDoseList: View {
     @ObservedObject var vaccineHistoricalViewModel = HistoricalVaccineData()
+    @State var viewState = CGSize.zero
+    @State var showCard = false
+    @State var bottomState = CGSize.zero
+    @State var showFull = false
+ 
 
     var body: some View {
  
-        NavigationView {
-        List {
-            ForEach(vaccineHistoricalViewModel.recentMDVaccineTotals?.features?.reversed() ?? [], id: \.self) { vaccine in
+                
+        VStack {
+     
+            ZStack {
+                
        
+                   
+                
+                BlurView(style: .systemUltraThinMaterial)
                     
-                    HStack {
-
+                    .frame(width: 400, height: showCard ? 900 : 400, alignment: .center)
+                    .cornerRadius(40)
+                    .ignoresSafeArea(showCard ? .all : .keyboard)
+                   
+                
+                VStack {
+                    
+                    ZStack {
                         
-            
-                        VStack(alignment: .leading, spacing: 12) {
-                           
+                        BlurView(style: .systemUltraThinMaterialDark)
                             
-                            HStack(spacing: 40) {
-                                 
-                                
-                               
-                                
-                                Text("\((vaccine.attributes?.CumulativeTotalVaccinatedDate?.getDateFromCrazyInt(date: vaccine.attributes!.CumulativeTotalVaccinatedDate!))!):")
-                                        .font(.system(size: 20, weight: .bold))
+                            .frame(width: 300, height: 50, alignment: .center)
+                            .cornerRadius(20)
+                    
+                    Text("History of Second Doses Vaccines")
+                        .bold()
                        
-                     
-                                VStack(alignment: .leading) {
-                                    Text("\(vaccine.attributes?.CumulativeTotalVaccinatedDoseTw ?? 0)")
-                                            .font(.system(size: 20, weight: .semibold))
+                        
+                        
+                        
+                        
+                    }
+                    .offset(y: showCard ? 50 : 0)
+                    .padding()
+                
+                List {
+                
+            
+                        VStack {
+                        ForEach(vaccineHistoricalViewModel.recentMDVaccineTotals?.features?.reversed() ?? [], id: \.self) { vaccine in
+                   
+                                
+                                HStack {
+
+                                    
+                        
+                                    VStack(alignment: .leading, spacing: 12) {
                                        
                                         
-                                }
+                                        HStack(spacing: 40) {
+                                             
+                                            
+                                           
+                                            
+                                            Text("\((vaccine.attributes?.CumulativeTotalVaccinatedDate?.getDateFromCrazyInt(date: vaccine.attributes!.CumulativeTotalVaccinatedDate!))!):")
+                                                    .font(.system(size: 20, weight: .bold))
+                                   
+                                 
+                                            VStack(alignment: .leading) {
+                                                Text("\(vaccine.attributes?.CumulativeTotalVaccinatedDoseTw ?? 0)")
+                                                        .font(.system(size: 20, weight: .semibold))
+                                                
+                                             
+                                            }
 
-                            }
-                          
-                            
-                            
+                                        }
+                                    }
+                                }
+                                .padding(.vertical, 8)
+                        }
                         
- 
                             
                         }
-                    }
-                    .padding(.vertical, 8)
+                 
+              
+                }
                 
+            
+                
+                
+                .frame(maxWidth: showCard ? 340 : 340)
+                .frame(maxHeight: showCard ? 800 : 220)
+             //   .background(Color.black)
+        //              .cornerRadius(20)
+                .clipShape(RoundedRectangle(cornerRadius: showCard ? 30 : 20, style: .continuous))
+                .shadow(radius: 20)
+                .offset(x: viewState.width, y: viewState.height)
+                .offset(y: showCard ? 80 : 0)
+                .blendMode(.hardLight)
+                .animation(.spring(response: 0.3, dampingFraction: 0.6, blendDuration: 0))
+                .onTapGesture {
+                    showCard.toggle()
+                  
+            
             }
-          
-        }
-       
-        .navigationBarTitle("History of Second Doses Given")
-        .navigationBarTitleDisplayMode(.inline)
-      
+
+                
+           
+            }
+            }
         }
     
 }
