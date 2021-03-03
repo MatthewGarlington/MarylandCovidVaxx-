bbfe4e8aa28ac27c78dad2c957eaad10ed12ac1f
Matthew Garlington <SBMatthewG@gmail.com>
2021-03-03 10:45:19 -0500

    style: Added Styling for the DeathLsit to present in card view
    The card view presented cna be expanded with a tap with a custom spring animation. The user can scroll through both list whether contracted or expanded

--- CovidVaccMD/View/Cases/DeathList.swift
+++ CovidVaccMD/View/Cases/DeathList.swift
@@ -9,106 +9,127 @@ import SwiftUI
 
 struct DeathList: View {
     @ObservedObject var casesModel = CovidMasterModel()
+    @State var viewState = CGSize.zero
+    @State var showCard = false
+    @State var bottomState = CGSize.zero
+    @State var showFull = false
+ 
 
     var body: some View {
-        NavigationView {
-        List {
-            ForEach(casesModel.allDeaths.reversed(), id: \.self) { deaths in
-       
-                    
-                    HStack {
-
-                        
-            
-                        VStack(alignment: .leading, spacing: 12) {
-                           
-                            
-                            VStack(alignment: .leading, spacing: 10) {
-
-
-
-
-                                Text("\((deaths.reportdate?.getDateFormatterForString(dateString: deaths.reportdate ?? ""))!):")
-                                    .font(.system(size: 20, weight: .bold))
-
-
-                          
-
-                                VStack(alignment: .leading) {
-                                    HStack(spacing: 10) {
-                                        HStack {
-                                            
-                                            Text("Total Deaths:")
-                                                .bold()
-                                            
-                                            Text("\(deaths.deaths ?? "")")
-                                                .font(.system(size: 20, weight: .semibold))
-                                        }
-
-
-
-                                    VStack(alignment: .leading) {
-                                        Text("+\(deaths.deathsdelta ?? "")")
-                                            .font(.caption)
-                                            .fontWeight(.bold)
-                                            .foregroundColor(.secondary)
-                                        Spacer()
-                                    }
-                                    }
-                                    
-//                                    HStack(spacing: 10) {
-//                                        HStack {
-//
-//                                            Text("Total in ICU:")
-//                                                .bold()
-//
-//                                            Text("\(cases.bedsicu ?? "")")
-//                                                .font(.system(size: 20, weight: .semibold))
-//                                        }
-//
-//
-//
-//                                    }
-//
-//                                    HStack(spacing: 10) {
-//                                        HStack {
-//
-//                                            Text("Total in Acute Care:")
-//                                                .bold()
-//
-//                                            Text("\(cases.bedsacute ?? "")")
-//                                                .font(.system(size: 20, weight: .semibold))
-//                                        }
-//
-//
-//
-//                                    }
-                                }
-                            }
-                          
-                            
-                            
-                        
  
+                
+        VStack {
+     
+            ZStack {
+                
+       
+                   
+                
+                BlurView(style: .systemUltraThinMaterial)
+                    
+                    .frame(width: 400, height: showCard ? 780 : 400, alignment: .center)
+                    .cornerRadius(40)
+                    .offset(y: showCard ? -20 : 0)
+                    .ignoresSafeArea(showCard ? .all : .keyboard)
+                 
+                   
+                
+                VStack {
+                    
+                    ZStack {
+                        
+                        BlurView(style: .systemUltraThinMaterialDark)
+                            
+                            .frame(width: 250, height: 50, alignment: .center)
+                            .cornerRadius(20)
+                    
+                    Text("History of Total Deaths")
+                        .bold()
+                       
+                        
+                        
+                        
+                        
+                    }
+                    .offset(y: showCard ? -40 : 0)
+                    .animation(.spring(response: 0.3, dampingFraction: 0.6, blendDuration: 0))
+                    .padding()
+                
+                List {
+                
+            
+                    VStack {
+                        ForEach(casesModel.allDeaths.reversed(), id: \.self) { deaths in
+                   
+                                
+                                VStack {
+
+                                    Text("\(deaths.reportdate?.getDateFormatterForString(dateString: deaths.reportdate ?? "") ?? ""):")
+                                        .font(.system(size: 20, weight: .bold))
+
+                                    Spacer()
+                                    
+
+                                    VStack(alignment: .leading, spacing: 12) {
+                                        HStack(spacing: 10) {
+                                            HStack {
+
+                                                Text("Total Deaths:")
+                                                    .font(.system(size: 20, weight: .bold))
+
+                                                Text("\(deaths.deaths ?? "")")
+                                                    .font(.system(size: 20, weight: .semibold))
+                                            }
+
+
+
+                                        VStack(alignment: .leading) {
+                                            Text("+\(deaths.deathsdelta ?? "")")
+                                                .font(.caption)
+                                                .fontWeight(.bold)
+                                                .foregroundColor(.primary)
+                                            Spacer()
+                                        }
+                                        }
+                                    }
+                                }
+                                .padding(.vertical, 8)
+                        }
+                            
                             
                         }
-                    }
-                    .padding(.vertical, 8)
+                 
+              
+                }
                 
+            
+                
+                
+                .frame(maxWidth: showCard ? 340 : 340)
+                .frame(maxHeight: showCard ? 600 : 220)
+             //   .background(Color.black)
+        //              .cornerRadius(20)
+                .clipShape(RoundedRectangle(cornerRadius: showCard ? 30 : 20, style: .continuous))
+                .shadow(radius: 20)
+                .offset(x: viewState.width, y: viewState.height)
+                .offset(y: showCard ? -40 : 0)
+                .blendMode(.hardLight)
+                .animation(.spring(response: 0.3, dampingFraction: 0.6, blendDuration: 0))
+                .onTapGesture {
+                    showCard.toggle()
+                  
+            
             }
-            .navigationBarTitle("History of Total Deaths")
-            .navigationBarTitleDisplayMode(.inline)
-        }
-       
-    
-      
-        }
-    
 
-
+                
+           
+            }
+            }
+        }
     
 }
 }
+
 struct DeathList_Previews: PreviewProvider {
     static var previews: some View {
         DeathList()
